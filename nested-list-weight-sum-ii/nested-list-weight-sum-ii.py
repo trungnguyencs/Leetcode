f0a27c8e5005fb22c8eaa61ad35f7f5ce09464b1
Trung Nguyen <32110646+trungnguyencs@users.noreply.github.com>
2021-01-15 18:30:25 -0800

    Time: 24 ms (96.31%), Space: 14.7 MB (23.60%) - LeetHub

---
+++ nested-list-weight-sum-ii/nested-list-weight-sum-ii.py
@@ -0,0 +1,57 @@
+# """
+# This is the interface that allows for creating nested lists.
+# You should not implement it, or speculate about its implementation
+# """
+#class NestedInteger:
+#    def __init__(self, value=None):
+#        """
+#        If value is not specified, initializes an empty list.
+#        Otherwise initializes a single integer equal to value.
+#        """
+#
+#    def isInteger(self):
+#        """
+#        @return True if this NestedInteger holds a single integer, rather than a nested list.
+#        :rtype bool
+#        """
+#
+#    def add(self, elem):
+#        """
+#        Set this NestedInteger to hold a nested list and adds a nested integer elem to it.
+#        :rtype void
+#        """
+#
+#    def setInteger(self, value):
+#        """
+#        Set this NestedInteger to hold a single integer equal to value.
+#        :rtype void
+#        """
+#
+#    def getInteger(self):
+#        """
+#        @return the single integer that this NestedInteger holds, if it holds a single integer
+#        Return None if this NestedInteger holds a nested list
+#        :rtype int
+#        """
+#
+#    def getList(self):
+#        """
+#        @return the nested list that this NestedInteger holds, if it holds a nested list
+#        Return None if this NestedInteger holds a single integer
+#        :rtype List[NestedInteger]
+#        """
+​
+class Solution:
+    def depthSumInverse(self, nestedList: List[NestedInteger]) -> int:
+        dic = defaultdict(int)
+        q = deque()
+        q.append([nestedList, 1])
+        while q:
+            cur, lv = q.popleft()
+            for element in cur:
+                if element.isInteger():
+                    dic[lv] += element.getInteger()
+                else:
+                    q.append([element.getList(), lv + 1])
+        return sum([(lv - k + 1) * v for k, v in dic.items()])
+        
