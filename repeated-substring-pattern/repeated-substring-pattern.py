9d77d19e75b5f97e69a368d1ac9b17af3864acb4
Trung Nguyen <32110646+trungnguyencs@users.noreply.github.com>
2021-02-07 15:57:48 -0800

    Time: 52 ms (66.80%), Space: 14.4 MB (40.51%) - LeetHub

---
+++ repeated-substring-pattern/repeated-substring-pattern.py
@@ -0,0 +1,10 @@
+class Solution:
+    def repeatedSubstringPattern(self, s: str) -> bool:
+        for i in range(1, len(s)//2 + 1):
+            if len(s) % i == 0 and s[:i] * (len(s)//i) == s:
+                return True
+        return False
+    
+        # trick solution:
+        # return s in (s + s)[1:-1]
+    
