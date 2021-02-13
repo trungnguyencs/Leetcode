1b8ed5b4e67106ff970ba77be1da8a57a08eeccf
Trung Nguyen <32110646+trungnguyencs@users.noreply.github.com>
2021-02-13 11:12:27 -0800

    Time: 28 ms (87.87%), Space: 14.2 MB (78.78%) - LeetHub

--- evaluate-division/evaluate-division.py
+++ evaluate-division/evaluate-division.py
@@ -1,22 +1,25 @@
 class Solution:
-    def calcEquation(self, equations: List[List[str]], values: List[float], queries: List[List[str]]) -> List[float]:
-        G = defaultdict(list)
-        for i in range(len(equations)):
-            s, e = equations[i]
-            G[s].append((e, values[i]))
-            G[e].append((s, 1/values[i]))
-        return [self.answerQuery(G, s, e) for s, e in queries]
-        
-    def answerQuery(self, G, s, e):
-        if s not in G or e not in G: return -1
-        q = deque([s])
-        visited = {s: 1.0}
-        while q:
-            cur = q.popleft()
-            if cur == e:
-                return visited[cur]
-            for neigh, multiplier in G[cur]:
-                if neigh not in visited:
-                    visited[neigh] = visited[cur] * multiplier
-                    q.append((neigh))
-        return -1
+    def calcEquation(self, equations: List[List[str]], values: List[float], queries: List[List[str]]) -> List[float]:
+        G = self.buildGraph(equations, values)
+        return [self.answerQuery(s, e, G) for s, e in queries]
+        
+    def buildGraph(self, equations, values):
+        G = defaultdict(list)
+        for equation, ratio in zip(equations, values):
+            s, e = equation
+            G[s].append((e, ratio))
+            G[e].append((s, 1.0/ratio))
+        return G
+    
+    def answerQuery(self, s, e, G):
+        if s not in G or e not in G: return -1
+        visited = {s: 1.0}
+        q = deque([s])
+        while q:
+            cur = q.popleft()
+            if cur == e: return visited[cur]
+            for nxt, multiplier in G[cur]:
+                if nxt not in visited:
+                    q.append(nxt)
+                    visited[nxt] = visited[cur] * multiplier
+        return -1
