19f4af8c1acec0bc58718d05e36124eaed100751
Trung Nguyen <32110646+trungnguyencs@users.noreply.github.com>
2021-02-28 22:23:59 -0800

    Time: 1448 ms (100.00%), Space: 20.7 MB (100.00%) - LeetHub

--- equal-sum-arrays-with-minimum-number-of-operations/equal-sum-arrays-with-minimum-number-of-operations.py
+++ equal-sum-arrays-with-minimum-number-of-operations/equal-sum-arrays-with-minimum-number-of-operations.py
@@ -1,21 +1,13 @@
 class Solution:
     def minOperations(self, nums1: List[int], nums2: List[int]) -> int:
-        s1, s2 = sum(nums1), sum(nums2)
-        if s1 == s2: return 0
-        elif s1 < s2: return self.helper(nums1, nums2, s2 - s1)
-        return self.helper(nums2, nums1, s1 - s2)
-    
-    def helper(self, small, large, diff):
-        small.sort(); large.sort(reverse=True)
-        i = j = 0
-        while True:
-            increment = 6 - small[i] if i < len(small) else 0
-            decrement = large[j] - 1 if i < len(large) else 0
-            if increment == decrement == 0: return -1
-            if increment >= decrement:
-                diff -= increment
-                i += 1
+        diff = sum(nums2) - sum(nums1)
+        if diff < 0: return self.minOperations(nums2, nums1)
+        counter = Counter([6-x for x in nums1] + [x-1 for x in nums2])
+        i, steps = 5, 0
+        while diff > 0:
+            if i == 0: return -1
+            if counter[i] == 0:
+                i -= 1
             else:
-                diff -= decrement
-                j += 1
-            if diff <= 0: return i + j
+                diff, counter[i], steps = diff-i, counter[i]-1, steps+1
+        return steps
