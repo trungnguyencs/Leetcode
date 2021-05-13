bba2f382f91cdc121cf8a7c7deb1ce442f39f50a
Trung Nguyen <32110646+trungnguyencs@users.noreply.github.com>
2021-05-12 17:49:26 -0700

    Time: 100 ms (96.00%), Space: 17.3 MB (53.93%) - LeetHub

--- range-sum-query-2d-immutable/range-sum-query-2d-immutable.py
+++ range-sum-query-2d-immutable/range-sum-query-2d-immutable.py
@@ -1,26 +1,21 @@
 class NumMatrix:
-​
-    def __init__(self, matrix: List[List[int]]):
-        if not matrix or not matrix[0]:
-            self.matrix = None
-            return
-        for r in range(1, len(matrix)):
-            matrix[r][0] += matrix[r-1][0]
-        for c in range(1, len(matrix[0])):
-            matrix[0][c] += matrix[0][c-1]
-        for r in range(1, len(matrix)):
-            for c in range(1, len(matrix[0])):
-                matrix[r][c] += matrix[r-1][c] + matrix[r][c-1] - matrix[r-1][c-1]
-        self.matrix = matrix
-        
-    def sumRegion(self, r1: int, c1: int, r2: int, c2: int) -> int:
-        if not self.matrix: return 0
-        upper = self.matrix[r1-1][c2] if r1 > 0 else 0
-        left = self.matrix[r2][c1-1] if c1 > 0 else 0
-        upperleft = self.matrix[r1-1][c1-1] if r1 > 0 and c1 > 0 else 0
-        return self.matrix[r2][c2] - upper - left + upperleft
-​
-​
+
+    def __init__(self, matrix: List[List[int]]):
+        for r in range(len(matrix)):
+            for c in range(len(matrix[0])):
+                upleft = 0 if r == 0 or c == 0 else matrix[r-1][c-1]
+                upright = 0 if r == 0 else matrix[r-1][c]
+                downleft = 0 if c == 0 else matrix[r][c-1]
+                matrix[r][c] += downleft + upright - upleft
+        self.matrix = matrix
+
+    def sumRegion(self, r1: int, c1: int, r2: int, c2: int) -> int:
+        upleft = 0 if r1 == 0 or c1 == 0 else self.matrix[r1-1][c1-1]
+        upright = 0 if r1 == 0 else self.matrix[r1-1][c2]
+        downleft = 0 if c1 == 0 else self.matrix[r2][c1-1]
+        return self.matrix[r2][c2] - downleft - upright + upleft
+
+
 # Your NumMatrix object will be instantiated and called as such:
 # obj = NumMatrix(matrix)
 # param_1 = obj.sumRegion(row1,col1,row2,col2)
