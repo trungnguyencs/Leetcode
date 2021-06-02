78dafcf05924f8c0ec3b4505ab4d49db43dbfe8b
Trung Nguyen <32110646+trungnguyencs@users.noreply.github.com>
2021-06-01 20:57:07 -0700

    Time: 60 ms (60.27%), Space: 14.2 MB (76.05%) - LeetHub

--- paint-house/paint-house.py
+++ paint-house/paint-house.py
@@ -1,7 +1,6 @@
 class Solution:
     def minCost(self, costs: List[List[int]]) -> int:
-        for r in range(1, len(costs)):
-            costs[r][0] += min(costs[r-1][1], costs[r-1][2])
-            costs[r][1] += min(costs[r-1][0], costs[r-1][2])
-            costs[r][2] += min(costs[r-1][0], costs[r-1][1])
-        return min(costs[-1])
+        r, g, b = costs[0]
+        for row in range(1, len(costs)):
+            r, g, b = costs[row][0] + min(g, b), costs[row][1] + min(r, b), costs[row][2] + min(r, g)
+        return min(r, g, b)
