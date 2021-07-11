2c3ff661a84ad2fa6eaffef2d929ce0ede6b3f23
Trung Nguyen <32110646+trungnguyencs@users.noreply.github.com>
2021-07-11 03:46:46 -0700

    Time: 1044 ms (29.38%), Space: 85.2 MB (9.47%) - LeetHub

--- first-unique-number/first-unique-number.py
+++ first-unique-number/first-unique-number.py
@@ -1,41 +1,49 @@
 class FirstUnique:
-​
-    def __init__(self, nums: List[int]):
-        self.dl = DL()
-        self.dic = defaultdict(Node)
-        for num in nums:
-            self.add(num)
-        
-    def showFirstUnique(self) -> int:
-        return self.dl.head.next.val
-​
-    def add(self, num: int) -> None:
-        if num not in self.dic:
-            self.dic[num] = self.dl.addback(num)
-        elif self.dic[num]:
-            self.dl.remove(self.dic[num])
-            self.dic[num] = None
-            
+
+    def __init__(self, nums: List[int]):
+        self.dic = {}
+        self.dl = DL()
+        for num in nums:
+            self.add(num)
+
+    def showFirstUnique(self) -> int:
+        return self.dl.head.next.val
+
+    def add(self, val: int) -> None:
+        if val not in self.dic:
+            node = Node(val)
+            self.dic[val] = node
+            self.dl.addBack(node)
+        elif self.dic[val]:
+            self.dl.remove(self.dic[val])
+            self.dic[val] = None
+            
+class Node:
+    
+    def __init__(self, val, prev=None, next=None):
+        self.val = val
+        self.prev = prev
+        self.next = next
+
 class DL:
-    
-    def __init__(self):
-        self.head = Node()
-        self.tail = Node(prev=self.head)
-        self.head.next = self.tail
-        # self.print()
-        
-    def addback(self, num):
-        node = Node(val=num, next=self.tail, prev=self.tail.prev)
-        self.tail.prev.next = node
-        self.tail.prev = node
-        return node
-        # self.print()
-        
-    def remove(self, node):
-        node.prev.next = node.next
-        node.next.prev = node.prev
-        # self.print()
-        
-    def print(self):
-        head = self.head
-        arr = []
+    
+    def __init__(self):
+        self.head = Node(-1)
+        self.tail = Node(-1, prev=self.head)
+        self.head.next = self.tail
+        
+    def addBack(self, node):
+        node.next = self.tail
+        node.prev = self.tail.prev
+        self.tail.prev.next = node
+        self.tail.prev = node
+        
+    def remove(self, node):
+        node.prev.next = node.next
+        node.next.prev = node.prev
+        
+        
+# Your FirstUnique object will be instantiated and called as such:
+# obj = FirstUnique(nums)
+# param_1 = obj.showFirstUnique()
+# obj.add(value)
