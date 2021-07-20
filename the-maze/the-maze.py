1d43b7eb9988981951f600e6630f3934da44d77a
Trung Nguyen <32110646+trungnguyencs@users.noreply.github.com>
2021-07-19 21:06:14 -0700

    Time: 276 ms (78.83%), Space: 14.8 MB (85.43%) - LeetHub

---
+++ the-maze/the-maze.py
@@ -0,0 +1,17 @@
+class Solution:
+    def hasPath(self, maze: List[List[int]], start: List[int], destination: List[int]) -> bool:
+        q = deque([start])
+        visited = set([tuple(start)])
+        while q:
+            r, c = q.popleft()
+            if [r, c] == destination: return True
+            directions = [[-1, 0], [1, 0], [0, -1], [0, 1]]
+            for dr, dc in directions:
+                nr, nc = r, c
+                while 0 <= nr + dr < len(maze) and 0 <= nc + dc < len(maze[0]) and maze[nr+dr][nc+dc] == 0:
+                    nr += dr
+                    nc += dc
+                if (nr, nc) not in visited:
+                    visited.add((nr, nc))
+                    q.append((nr, nc))
+        return False
