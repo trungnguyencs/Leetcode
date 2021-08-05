157b477fa9e67fc4160098d7fc3e998fd50d3204
Trung Nguyen <32110646+trungnguyencs@users.noreply.github.com>
2021-08-05 09:09:15 -0700

    Time: 396 ms (43.26%), Space: 14.9 MB (77.51%) - LeetHub

---
+++ the-maze-ii/the-maze-ii.py
@@ -0,0 +1,19 @@
+class Solution:
+    def shortestDistance(self, maze: List[List[int]], start: List[int], destination: List[int]) -> int:
+        minDistance = float('inf')
+        q = deque([start])
+        visited = {tuple(start): 0}
+        while q:
+            r, c = q.popleft()
+            directions = [[-1, 0], [1, 0], [0, -1], [0, 1]]
+            for dr, dc in directions:
+                nr, nc, nd = r, c, visited[(r, c)]
+                while 0 <= nr + dr < len(maze) and 0 <= nc + dc < len(maze[0]) and maze[nr + dr][nc + dc] == 0:
+                    nr += dr
+                    nc += dc
+                    nd += 1
+                if [nr, nc] == destination: minDistance = min(minDistance, nd)
+                if (nr, nc) not in visited or visited[(nr, nc)] > nd:
+                    visited[(nr, nc)] = nd
+                    q.append((nr, nc))
+        return -1 if minDistance == float('inf') else minDistance
