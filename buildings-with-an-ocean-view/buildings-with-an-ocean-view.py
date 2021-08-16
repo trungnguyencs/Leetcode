82d866129361a1fc528dc65c12c5c0c8400b0b4b
Trung Nguyen <32110646+trungnguyencs@users.noreply.github.com>
2021-08-15 20:25:59 -0700

    Time: 752 ms (35.94%), Space: 31.7 MB (56.80%) - LeetHub

--- buildings-with-an-ocean-view/buildings-with-an-ocean-view.py
+++ buildings-with-an-ocean-view/buildings-with-an-ocean-view.py
@@ -1,9 +1,9 @@
 class Solution:
     def findBuildings(self, heights: List[int]) -> List[int]:
-        maxHeight = heights[-1]
+        maxRight = heights[-1]
         ans = [len(heights) - 1]
-        for i in range(len(heights)-2, -1, -1):
-            if heights[i] > maxHeight:
+        for i in range(len(heights) - 2, -1, -1):
+            if heights[i] > maxRight:
                 ans.append(i)
-                maxHeight = heights[i]
+            maxRight = max(maxRight, heights[i])
         return ans[::-1]
