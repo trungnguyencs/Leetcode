965595b4eeaa5737399571391729d980f8720b9c
Trung Nguyen <32110646+trungnguyencs@users.noreply.github.com>
2021-12-01 18:44:10 -0800

    Time: 40 ms (86.95%), Space: 16.4 MB (56.86%) - LeetHub

--- odd-even-linked-list/odd-even-linked-list.py
+++ odd-even-linked-list/odd-even-linked-list.py
@@ -6,12 +6,12 @@
 class Solution:
     def oddEvenList(self, head: Optional[ListNode]) -> Optional[ListNode]:
         if not head or not head.next: return head
-        oddHead = odd = head
-        evenHead = even = head.next
-        while odd.next and odd.next.next:
+        odd = oddHead = head
+        even = evenHead = head.next
+        while even and even.next:
             odd.next = even.next
-            even.next = odd.next.next
+            even.next = even.next.next
             odd = odd.next
             even = even.next
         odd.next = evenHead
-        return head
+        return oddHead
