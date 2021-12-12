1fe50ff6f59ea835fc5b0177a122da1a7075832a
Trung Nguyen <32110646+trungnguyencs@users.noreply.github.com>
2021-12-11 17:33:01 -0800

    Time: 812 ms (67.65%), Space: 14.4 MB (82.02%) - LeetHub

--- partition-equal-subset-sum/partition-equal-subset-sum.py
+++ partition-equal-subset-sum/partition-equal-subset-sum.py
@@ -1,9 +1,9 @@
 class Solution:
     def canPartition(self, nums: List[int]) -> bool:
-        s = sum(nums)
-        if s % 2 == 1: return False
-        target = s//2
-        dp = [False]*(target + 1) # 2D dp matrix also works, similar to coin change
+        total = sum(nums)
+        if total % 2 == 1: return False
+        target = total//2
+        dp = [False]*(target + 1)
         dp[0] = True
         for num in nums:
             for i in range(target, num - 1, -1):
