44756e47db2196c00f1927757e06f81f410a51d9
Trung Nguyen <32110646+trungnguyencs@users.noreply.github.com>
2022-01-05 21:22:37 -0500

    Time: 123 ms (13.78%), Space: 14.6 MB (92.36%) - LeetHub

--- 1094-car-pooling/1094-car-pooling.py
+++ 1094-car-pooling/1094-car-pooling.py
@@ -1,9 +1,22 @@
 class Solution:
+    # def carPooling(self, trips: List[List[int]], capacity: int) -> bool:
+    #     events = []
+    #     for num, s, e in trips:
+    #         events.extend([[s, 1, num], [e, 0, num]]) # 1: start, 0: end
+    #     for time, event, num in sorted(events):
+    #         capacity += num if event == 0 else -num
+    #         if capacity < 0: return False
+    #     return True
+    
     def carPooling(self, trips: List[List[int]], capacity: int) -> bool:
-        events = []
+        trips.sort(key=lambda x: (x[1], x[2]))
+        heap = []
         for num, s, e in trips:
-            events.extend([[s, 1, num], [e, 0, num]]) # 1: start, 0: end
-        for time, event, num in sorted(events):
-            capacity += num if event == 0 else -num
-            if capacity < 0: return False
+            while heap and heap[0][0] <= s:
+                _, _, releasedNum = heappop(heap)
+                capacity += releasedNum
+            if capacity < num:
+                return False
+            capacity -= num
+            heappush(heap, (e, s, num)) # what ends first is on top of heap
         return True
