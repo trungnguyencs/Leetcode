a97c55062383fc0a9dad0c287eed6101c2fce060
Trung Nguyen <32110646+trungnguyencs@users.noreply.github.com>
2022-01-06 00:58:06 -0500

    Time: 521 ms (46.24%), Space: 16.8 MB (62.35%) - LeetHub

--- 524-longest-word-in-dictionary-through-deleting/524-longest-word-in-dictionary-through-deleting.py
+++ 524-longest-word-in-dictionary-through-deleting/524-longest-word-in-dictionary-through-deleting.py
@@ -1,11 +1,17 @@
 class Solution:
-    def findLongestWord(self, s: str, dictionary: List[str]) -> str:
-        ans = ''
-        for string in dictionary:
-            if self.isSubsequence(string, s):
-                ans = min(ans, string, key=lambda x: (-len(x), x))
-        return ans
+    # def findLongestWord(self, s: str, dictionary: List[str]) -> str:
+    #     ans = ''
+    #     for string in dictionary:
+    #         if self.isSubsequence(string, s):
+    #             ans = min(ans, string, key=lambda x: (-len(x), x))
+    #     return ans
         
+    def findLongestWord(self, s: str, d: List[str]) -> str:
+        d.sort(key=lambda x: (-len(x), x)) # front has max len, min lexicographical order
+        for word in d:
+            if self.isSubsequence(word, s): return word
+        return ''
+    
     def isSubsequence(self, short, long):
         i = 0
         for ch in long:
