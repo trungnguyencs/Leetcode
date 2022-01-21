139a98931c97814bff46ec7bc732730f53336ccc
Trung Nguyen <32110646+trungnguyencs@users.noreply.github.com>
2022-01-20 22:20:17 -0800

    Time: 92 ms (92.64%), Space: 15.4 MB (70.43%) - LeetHub

---
+++ 735-asteroid-collision/735-asteroid-collision.py
@@ -0,0 +1,15 @@
+class Solution:
+    def asteroidCollision(self, nums: List[int]) -> List[int]:
+        stack = []
+        for num in nums:
+            if num > 0:
+                stack.append(num)
+                continue
+            while stack and stack[-1] > 0 and stack[-1] < -num:
+                stack.pop()
+            if stack and stack[-1] == -num:
+                stack.pop()
+                continue
+            if not stack or stack[-1] < 0:
+                stack.append(num)
+        return stack
