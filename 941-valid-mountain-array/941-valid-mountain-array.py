c5ef44fe2131f2562cb62b2c664f5b67749d6737
Trung Nguyen <32110646+trungnguyencs@users.noreply.github.com>
2022-01-24 20:52:58 -0800

    Time: 228 ms (42.55%), Space: 15.6 MB (18.42%) - LeetHub

--- 941-valid-mountain-array/941-valid-mountain-array.py
+++ 941-valid-mountain-array/941-valid-mountain-array.py
@@ -1,13 +1,11 @@
 class Solution:
     def validMountainArray(self, arr: List[int]) -> bool:
-        if len(arr) < 3 or arr[0] >= arr[1] or arr[-1] >= arr[-2]:
+        if len(arr) < 3:
             return False
-        tipCount = 0
+        peak = valley = 0
         for i in range(1, len(arr) - 1):
-            if arr[i] == arr[i-1] or arr[i] == arr[i+1]:
-                return False
-            if arr[i] > arr[i-1] and arr[i] > arr[i+1]:
-                tipCount += 1
-                if tipCount > 1:
-                    return False
-        return True
+            if arr[i-1] < arr[i] > arr[i+1]:
+                peak += 1
+            if arr[i-1] >= arr[i] <= arr[i+1]:
+                valley += 1
+        return peak == 1 and valley == 0
