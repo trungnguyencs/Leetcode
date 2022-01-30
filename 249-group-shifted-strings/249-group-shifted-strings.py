d00d5ac6e6ff8688daba9abd3c9091c135e0223d
Trung Nguyen <32110646+trungnguyencs@users.noreply.github.com>
2022-01-30 10:46:57 -0800

    Time: 59 ms (25.43%), Space: 14 MB (98.55%) - LeetHub

--- 249-group-shifted-strings/249-group-shifted-strings.py
+++ 249-group-shifted-strings/249-group-shifted-strings.py
@@ -1,13 +1,13 @@
 class Solution:
-    def groupStrings(self, strings: List[str]) -> List[List[str]]:
+    def groupStrings(self, words: List[str]) -> List[List[str]]:
         dic = defaultdict(list)
-        for s in strings:
-            diff = [0]
-            for i in range(len(s) - 1):
-                diff.append(self.getDiffWarpedAround(s[i+1], s[i]))
-            dic[tuple(diff)].append(s)
+        for word in words:
+            dic[self.word2Distances(word)].append(word)
         return dic.values()
     
-    def getDiffWarpedAround(self, ch1, ch2):
-        ascii1, ascii2 = ord(ch1), ord(ch2)
-        return ascii2 - ascii1 if ascii2 >= ascii1 else 26 + ascii2 - ascii1
+    def word2Distances(self, word):
+        distances = [0]
+        for i in range(len(word)):
+            d = ord(word[i]) - ord(word[i-1])
+            distances.append(d if d >= 0 else 26 + d)
+        return tuple(distances)
