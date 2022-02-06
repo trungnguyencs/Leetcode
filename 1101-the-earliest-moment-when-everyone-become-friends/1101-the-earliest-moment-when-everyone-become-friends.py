002ef67397b2211a7bfedf5ccb01a6b31f4dc308
Trung Nguyen <32110646+trungnguyencs@users.noreply.github.com>
2022-02-06 09:41:34 -0800

    Time: 202 ms (20.62%), Space: 14.2 MB (99.11%) - LeetHub

--- 1101-the-earliest-moment-when-everyone-become-friends/1101-the-earliest-moment-when-everyone-become-friends.py
+++ 1101-the-earliest-moment-when-everyone-become-friends/1101-the-earliest-moment-when-everyone-become-friends.py
@@ -1,5 +1,5 @@
 class Solution:
-    def earliestAcq(self, logs: List[List[int]], N: int) -> int:
+    def earliestAcq(self, logs: List[List[int]],  N: int) -> int:
         self.groupCount = N
         self.parents = [i for i in range(N)]
         logs.sort(key=lambda x: x[0])
@@ -16,6 +16,13 @@ class Solution:
         return self.groupCount == 1 # return True when all nodes are connected
     
     def find(self, node):
-        while node != self.parents[node]:
-            node = self.parents[node]
-        return node
+        root = node
+        while root != self.parents[root]:
+            root = self.parents[root]
+        
+        # flatten the child -> root path for better perf
+        while node != root:
+            nextParent = self.parents[node]
+            self.parents[node] = root
+            node = nextParent
+        return root
