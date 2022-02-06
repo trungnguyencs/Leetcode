a4d5af8ef5ba8bf8e0727ec42fac355847985d2b
Trung Nguyen <32110646+trungnguyencs@users.noreply.github.com>
2022-02-06 09:29:29 -0800

    Time: 183 ms (31.84%), Space: 14.4 MB (71.00%) - LeetHub

---
+++ 1101-the-earliest-moment-when-everyone-become-friends/1101-the-earliest-moment-when-everyone-become-friends.py
@@ -0,0 +1,21 @@
+class Solution:
+    def earliestAcq(self, logs: List[List[int]], N: int) -> int:
+        self.groupCount = N
+        self.parents = [i for i in range(N)]
+        logs.sort(key=lambda x: x[0])
+        for time, x, y in logs:
+            if self.union(x, y):
+                return time
+        return -1
+        
+    def union(self, node1, node2):
+        parent1, parent2 = self.find(node1), self.find(node2)
+        if parent1 != parent2:
+            self.parents[parent2] = parent1
+            self.groupCount -= 1
+        return self.groupCount == 1 # return True when all nodes are connected
+    
+    def find(self, node):
+        while node != self.parents[node]:
+            node = self.parents[node]
+        return node
