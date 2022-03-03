8e691a817ef682e4adf47110e53048362a0b5622
Trung Nguyen <32110646+trungnguyencs@users.noreply.github.com>
2022-03-02 19:15:57 -0600

    Time: 52 ms (57.19%), Space: 14.1 MB (75.02%) - LeetHub

---
+++ 413-arithmetic-slices/413-arithmetic-slices.py
@@ -0,0 +1,11 @@
+class Solution:
+    def numberOfArithmeticSlices(self, nums: List[int]) -> int:
+        if len(nums) < 3: return 0
+        ans = streak = 0
+        for i in range(2, len(nums)):
+            if nums[i] - nums[i-1] == nums[i-1] - nums[i-2]:
+                streak += 1
+                ans += streak
+            else:
+                streak = 0
+        return ans
