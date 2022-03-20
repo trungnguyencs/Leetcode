d56a0e841fa0bf26a21ad12584d6b9ebe9be2383
Trung Nguyen <32110646+trungnguyencs@users.noreply.github.com>
2022-03-19 20:57:08 -0700

    Time: 1944 ms (16.64%), Space: 15.3 MB (22.57%) - LeetHub

--- 1007-minimum-domino-rotations-for-equal-row/1007-minimum-domino-rotations-for-equal-row.py
+++ 1007-minimum-domino-rotations-for-equal-row/1007-minimum-domino-rotations-for-equal-row.py
@@ -4,12 +4,13 @@ class Solution:
         return -1 if ans == float('inf') else ans
         
     def helper(self, tops, bottoms):
-        counter = Counter(tops)
-        mostPopular = max(counter.keys(), key=lambda x: counter[x])
-        flip = 0
+        topCount = bottomCount = 0
+        base = tops[0]
         for i in range(len(tops)):
-            if mostPopular not in [tops[i], bottoms[i]]:
+            if base not in [tops[i], bottoms[i]]:
                 return float('inf')
-            if tops[i] != mostPopular:
-                flip += 1
-        return flip
+            if tops[i] == base:
+                topCount += 1
+            if bottoms[i] == base:
+                bottomCount += 1
+        return min(len(tops) - topCount, len(tops) - bottomCount)
