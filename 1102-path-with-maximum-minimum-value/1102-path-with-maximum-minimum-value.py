b12e2b62a6cabe97bb63e07ebe836caa8011f9eb
Trung Nguyen <32110646+trungnguyencs@users.noreply.github.com>
2022-03-31 16:57:48 -0700

    Time: 1365 ms (85.67%), Space: 15.5 MB (85.03%) - LeetHub

---
+++ 1102-path-with-maximum-minimum-value/1102-path-with-maximum-minimum-value.py
@@ -0,0 +1,17 @@
+class Solution:
+    def maximumMinimumPath(self, A: List[List[int]]) -> int:
+        dire = [(0, 1), (1, 0), (0, -1), (-1, 0)]
+        R, C = len(A), len(A[0])
+        
+        maxHeap = [(-A[0][0], 0, 0)]
+        seen = [[0 for _ in range(C)] for _ in range(R)]
+        while maxHeap:
+            val, x, y = heapq.heappop(maxHeap)
+            # seen[x][y] = 1 # got TLE
+            if x == R - 1 and y == C - 1: return -val
+            for dx, dy in dire:
+                nx, ny = x + dx, y + dy
+                if 0 <= nx < R and 0 <= ny < C and not seen[nx][ny]:
+                    seen[nx][ny] = 1 # passed
+                    heapq.heappush(maxHeap, (max(val, -A[nx][ny]), nx, ny))
+        return -1
