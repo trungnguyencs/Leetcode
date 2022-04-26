62fa31cebbea326ae8f0e986703e0360d085852f
Trung Nguyen <32110646+trungnguyencs@users.noreply.github.com>
2022-04-25 23:23:07 -0700

    Time: 113 ms (7.99%), Space: 14.5 MB (35.50%) - LeetHub

---
+++ 242-valid-anagram/242-valid-anagram.py
@@ -0,0 +1,8 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        counter = Counter(s)
+        for ch in t:
+            if ch not in counter: return False
+            counter[ch] -= 1
+            if counter[ch] == 0: del counter[ch]
+        return not counter
