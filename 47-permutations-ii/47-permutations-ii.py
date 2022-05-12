1abc921c6357315228ff70768e5dcdf559e777f2
Trung Nguyen <32110646+trungnguyencs@users.noreply.github.com>
2022-05-12 16:56:25 -0700

    Time: 444 ms (18.02%), Space: 14.1 MB (77.62%) - LeetHub

---
+++ 47-permutations-ii/47-permutations-ii.py
@@ -0,0 +1,17 @@
+class Solution:
+    def permuteUnique(self, nums: List[int]) -> List[List[int]]:
+        self.ans = set()
+        self.backtrack(nums, [], set())
+        return self.ans
+        
+    def backtrack(self, nums, arr, visited):
+        if len(arr) == len(nums):
+            self.ans.add(tuple(arr))
+            return
+        for i, num in enumerate(nums):
+            if i in visited: continue
+            arr.append(num)
+            visited.add(i)
+            self.backtrack(nums, arr, visited)
+            arr.pop()
+            visited.remove(i)
