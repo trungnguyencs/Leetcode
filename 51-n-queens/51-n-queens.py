e222427ae8364c0033b8fb3e673883ed39b96e5c
Trung Nguyen <32110646+trungnguyencs@users.noreply.github.com>
2022-06-04 16:50:46 -0700

    Time: 67 ms (78.23%), Space: 14.6 MB (14.13%) - LeetHub

---
+++ 51-n-queens/51-n-queens.py
@@ -0,0 +1,18 @@
+class Solution:
+    def solveNQueens(self, n: int) -> List[List[str]]:
+        self.ans = []
+        board = [['.']*n for _ in range(n)]
+        self.backtrack(board, 0, set(), set(), set())
+        return self.ans
+        
+    def backtrack(self, board, r, cols, diag, codiag):
+        if r == len(board):
+            self.ans.append([''.join(row) for row in board])
+            return
+        for c in range(len(board[0])):
+            if c in cols or r + c in diag or r - c in codiag: continue
+            board[r][c] = 'Q'
+            cols.add(c); diag.add(r + c); codiag.add(r - c)
+            self.backtrack(board, r + 1, cols, diag, codiag)
+            board[r][c] = '.'
+            cols.remove(c); diag.remove(r + c); codiag.remove(r - c)         
