214c3deaaf86eca2f4cacaf48d89cdd980d661ca
Trung Nguyen <32110646+trungnguyencs@users.noreply.github.com>
2022-06-04 21:19:03 -0700

    Time: 2197 ms (20.00%), Space: 69.1 MB (40.00%) - LeetHub

--- 2295-replace-elements-in-an-array/2295-replace-elements-in-an-array.py
+++ 2295-replace-elements-in-an-array/2295-replace-elements-in-an-array.py
@@ -1,9 +1,19 @@
 class Solution:
     def arrayChange(self, nums: List[int], operations: List[List[int]]) -> List[int]:
-        swaps = {}
-        for s, e in reversed(operations):
-            swaps[s] = swaps[e] if e in swaps else e
-        for i, num in enumerate(nums):
-            if num in swaps:
-                nums[i] = swaps[num]
+        # # swaps
+        # swaps = {}
+        # for s, e in reversed(operations):
+        #     swaps[s] = swaps[e] if e in swaps else e
+        # for i, num in enumerate(nums):
+        #     if num in swaps:
+        #         nums[i] = swaps[num]
+        # return nums
+        
+        # index mapping
+        dic = {num: i for i, num in enumerate(nums)}
+        for s, e in operations:
+            i = dic[s]
+            nums[i] = e
+            dic[e] = i
+            del dic[s]
         return nums
