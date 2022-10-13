03cf19fe6b480996b03502b161fcab3724f9fc5a
Thanh Trung Nguyen <trung@Thanhs-MacBook-Air.local>
2022-10-12 22:43:18 -0400

    pad zeros to file names

--- utils/rename.py
+++ utils/rename.py
@@ -1,8 +1,18 @@
 import os
 
+def padZeros(name):
+    lst = name.split('-')
+    lst[0] = lst[0].zfill(4)
+    return '-'.join(lst)
+
 folderLst = os.listdir()
 for folder in folderLst:
-	lst = folder.split('-')
-	lst[0] = lst[0].zfill(4)
-	newName = '-'.join(lst)
-	os.rename(folder, newName)
+    os.rename(folder, padZeros(folder))
+
+
+for folder in folderLst:
+    if not folder.split('-')[0].isnumeric(): continue
+    for path, subdirs, files in os.walk(folder):
+        for name in files:
+            if name.endswith('.py'):
+                os.rename(os.path.join(path, name), os.path.join(path, padZeros(name)))
