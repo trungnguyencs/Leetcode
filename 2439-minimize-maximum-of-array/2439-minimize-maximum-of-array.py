622ee8710be89fe7c236bc262d36def455ec6130
Trung Nguyen <32110646+trungnguyencs@users.noreply.github.com>
2023-04-05 13:30:15 -0700

    Time: 815 ms (68.30%), Space: 26 MB (84.38%) - LeetHub

--- 2439-minimize-maximum-of-array/2439-minimize-maximum-of-array.py
+++ 2439-minimize-maximum-of-array/2439-minimize-maximum-of-array.py
@@ -4,5 +4,5 @@ class Solution:
         ans = float('-inf')
         for i, num in enumerate(nums):
             prefixSum += num
-            ans = max(ans, math.ceil(prefixSum/(i + 1))) # min max when all numbers are equal to their average
+            ans = max(ans, math.ceil(prefixSum/(i + 1))) # min max when all numbers are equal to their average, but we can only average number before it, not after it -> prefix sum
         return ans
