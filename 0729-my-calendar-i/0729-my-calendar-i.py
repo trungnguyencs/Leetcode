3f8f45b2c252b6fa3d961cc62aa9c09bd612c500
Trung Nguyen <32110646+trungnguyencs@users.noreply.github.com>
2023-04-25 02:45:06 -0700

    Time: 282 ms (48.06%), Space: 15 MB (22.64%) - LeetHub

--- 0729-my-calendar-i/0729-my-calendar-i.py
+++ 0729-my-calendar-i/0729-my-calendar-i.py
@@ -5,15 +5,31 @@ class MyCalendar:
     def __init__(self):
         self.dic = SortedDict()
 
+    # def book(self, start: int, end: int) -> bool:
+    #     keys = self.dic.keys()
+    #     if keys:
+    #         r = self.dic.bisect_right(start)
+    #         if r != 0:
+    #             floorKey = self.dic[keys[r-1]]
+    #             if floorKey > start: return False
+    #         l = self.dic.bisect_left(start)
+    #         if l != len(keys):
+    #             ceilKey = keys[l]
+    #             if end > ceilKey: return False
+    #     self.dic[start] = end
+    #     return True
+        
     def book(self, start: int, end: int) -> bool:
         keys = self.dic.keys()
         if keys:
-            l = self.dic.bisect_left(start)
-            if l != len(keys):
-                if end > keys[l]: return False
-            r = self.dic.bisect_right(start)
-            if r != 0:
-                if self.dic[keys[r-1]] > start: return False
+            i = self.dic.bisect_right(start)
+            if i != 0:
+                floorKey = self.dic[keys[i-1]]
+                if floorKey > start: return False
+            if i == len(keys) and start == keys[-1]: i -= 1 # do this equivalent to bisect_left() to save time
+            if i != len(keys):
+                ceilKey = keys[i]
+                if end > ceilKey: return False
         self.dic[start] = end
         return True
 
