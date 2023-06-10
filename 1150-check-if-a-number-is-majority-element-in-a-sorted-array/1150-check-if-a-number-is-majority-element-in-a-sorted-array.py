79d83447dfad41ef9b6232fe22a8a130800e8291
Trung Nguyen <32110646+trungnguyencs@users.noreply.github.com>
2023-06-10 10:13:45 -0700

    Time: 61 ms (8.85%), Space: 16.4 MB (82.89%) - LeetHub

--- 1150-check-if-a-number-is-majority-element-in-a-sorted-array/1150-check-if-a-number-is-majority-element-in-a-sorted-array.py
+++ 1150-check-if-a-number-is-majority-element-in-a-sorted-array/1150-check-if-a-number-is-majority-element-in-a-sorted-array.py
@@ -1,9 +1,9 @@
 class Solution:
     def isMajorityElement(self, nums: List[int], target: int) -> bool:
         left = self.findLeftMostIndex(nums, target)
-        if left == -1: return False
-        right = left + len(nums)//2
-        return right < len(nums) and nums[right] == target
+        right = self.findRightMostIndex(nums, target)
+        if -1 in [left, right]: return False
+        return right - left + 1 > len(nums)//2
         
     def findLeftMostIndex(self, nums, target):
         l, r = 0, len(nums) - 1
@@ -15,6 +15,20 @@ class Solution:
                 l = m + 1
             elif m > 0 and nums[m-1] == target:
                 r = m - 1
+            else:
+                return m
+        return -1
+    
+    def findRightMostIndex(self, nums, target):
+        l, r = 0, len(nums) - 1
+        while l <= r:
+            m = (l + r)//2
+            if nums[m] > target:
+                r = m - 1
+            elif nums[m] < target:
+                l = m + 1
+            elif m < len(nums) - 1 and nums[m+1] == target:
+                l = m + 1
             else:
                 return m
         return -1
