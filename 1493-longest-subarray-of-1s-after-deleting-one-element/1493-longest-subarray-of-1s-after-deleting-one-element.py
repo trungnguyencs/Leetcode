4f0d0c321ddc00203286eb53e219ab420b8b6283
Trung Nguyen <32110646+trungnguyencs@users.noreply.github.com>
2023-07-04 21:26:51 -0700

    Time: 400 ms (41.96%), Space: 19.2 MB (11.96%) - LeetHub

---
+++ 1493-longest-subarray-of-1s-after-deleting-one-element/1493-longest-subarray-of-1s-after-deleting-one-element.py
@@ -0,0 +1,13 @@
+class Solution:
+    def longestSubarray(self, nums: List[int]) -> int:
+        # same as longest window with at most one 0
+        longest = l = zeroCount = 0
+        for r, num in enumerate(nums):
+            if num == 0:
+                zeroCount += 1
+                while zeroCount == 2:
+                    if nums[l] == 0:
+                        zeroCount -= 1
+                    l += 1
+            longest = max(longest, r - l + 1)
+        return longest - 1
