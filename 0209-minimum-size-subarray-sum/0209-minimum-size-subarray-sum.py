a6e918974a2c1edfc1788db90b49f6000a01815e
Trung Nguyen <32110646+trungnguyencs@users.noreply.github.com>
2023-07-06 16:31:15 -0700

    Time: 337 ms (5.06%), Space: 29.1 MB (41.03%) - LeetHub

--- 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.py
+++ 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.py
@@ -1,14 +1,23 @@
 class Solution:
     def minSubArrayLen(self, target: int, nums: List[int]) -> int:
         # O(n): Sliding Window
-        minWindow = float('inf')
-        s = l = 0
-        for r, num in enumerate(nums):
-            s += num
-            while s >= target:
-                minWindow = min(minWindow, r - l + 1)
-                s -= nums[l]
-                l += 1
-        return 0 if minWindow == float('inf') else minWindow
+        # minWindow = float('inf')
+        # s = l = 0
+        # for r, num in enumerate(nums):
+        #     s += num
+        #     while s >= target:
+        #         minWindow = min(minWindow, r - l + 1)
+        #         s -= nums[l]
+        #         l += 1
+        # return 0 if minWindow == float('inf') else minWindow
         
         # O(nlogn): Prefix Sum + Binary Search
+        minWindow = float('inf')
+        for i in range(1, len(nums)):
+            nums[i] += nums[i-1]
+        for l in range(len(nums)):
+            bisectTarget = target + (nums[l-1] if l > 0 else 0)
+            r = bisect_left(nums, bisectTarget)
+            if r != len(nums):
+                minWindow = min(minWindow, r - l + 1)
+        return 0 if minWindow == float('inf') else minWindow
