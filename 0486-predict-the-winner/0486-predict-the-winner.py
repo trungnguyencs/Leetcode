802766d15e3d68a6d340da079e395f5c6ce40654
Trung Nguyen <32110646+trungnguyencs@users.noreply.github.com>
2023-07-27 18:42:25 -0700

    Time: 44 ms (92.84%), Space: 16.6 MB (19.78%) - LeetHub

---
+++ 0486-predict-the-winner/0486-predict-the-winner.py
@@ -0,0 +1,7 @@
+class Solution:
+    def PredictTheWinner(self, nums: List[int]) -> bool:
+        @cache
+        def dp(i, j):
+            if i == j: return nums[i]
+            return max(nums[i] - dp(i + 1, j), nums[j] - dp(i, j - 1))
+        return dp(0, len(nums) - 1) >= 0
