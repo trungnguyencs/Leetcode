fbed5d59f6a133bc5b5a01b606d5945659ef3460
Trung Nguyen <32110646+trungnguyencs@users.noreply.github.com>
2023-08-28 20:21:56 -0700

    Time: 97 ms (95.48%), Space: 17.4 MB (73.49%) - LeetHub

--- 2483-minimum-penalty-for-a-shop/2483-minimum-penalty-for-a-shop.py
+++ 2483-minimum-penalty-for-a-shop/2483-minimum-penalty-for-a-shop.py
@@ -1,15 +1,26 @@
 class Solution:
+    # # 2 passes: find minimum penalty
+    # def bestClosingTime(self, s: str) -> int:
+    #     totalY = s.count('Y')
+    #     totalN = len(s) - totalY
+    #     curY, curN = 0, totalN
+    #     minPenalty, ans = len(s), 0
+    #     for i, c in enumerate(s):
+    #         if c == 'Y':
+    #             curY += 1
+    #         else:
+    #             curN += 1
+    #         if curN + (totalY - curY) < minPenalty:
+    #             minPenalty = curN + (totalY - curY)
+    #             ans = i + 1
+    #     return ans
+    
+    # 1 pass: find maximum profit
     def bestClosingTime(self, s: str) -> int:
-        totalY = s.count('Y')
-        totalN = len(s) - totalY
-        curY, curN = 0, totalN
-        minPenalty, ans = len(s), 0
+        profit = maxProfit = ans = 0
         for i, c in enumerate(s):
-            if c == 'Y':
-                curY += 1
-            else:
-                curN += 1
-            if curN + (totalY - curY) < minPenalty:
-                minPenalty = curN + (totalY - curY)
+            profit += (1 if c == 'Y' else -1)
+            if profit > maxProfit:
+                maxProfit = profit
                 ans = i + 1
         return ans
