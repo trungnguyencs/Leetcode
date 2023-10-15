5b1e3c595ed36d3e042c6d15217cfd33b4d9fd42
Trung Nguyen <32110646+trungnguyencs@users.noreply.github.com>
2023-10-14 23:52:39 -0600

    Time: 47 ms (16.08%), Space: 16.3 MB (59.41%) - LeetHub

--- 0409-longest-palindrome/0409-longest-palindrome.py
+++ 0409-longest-palindrome/0409-longest-palindrome.py
@@ -2,9 +2,6 @@ class Solution:
     def longestPalindrome(self, s: str) -> int:
         counter = Counter(s)
         ans = 0
-        for num, freq in counter.items():
-            if freq % 2 == 0 or ans % 2 == 0:
-                ans += freq
-            else:
-                ans += freq - 1
+        for ch, freq in counter.items():
+            ans += freq if (freq % 2 == 0 or ans % 2 == 0) else freq - 1
         return ans
