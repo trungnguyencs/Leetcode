b2b0cdc403388e3c93510e187ce77676d0ea3ff5
Trung Nguyen <32110646+trungnguyencs@users.noreply.github.com>
2023-11-19 05:05:16 -0700

    Time: 938 ms (12.89%), Space: 30.9 MB (6.19%) - LeetHub

---
+++ 1887-reduction-operations-to-make-the-array-elements-equal/1887-reduction-operations-to-make-the-array-elements-equal.py
@@ -0,0 +1,8 @@
+class Solution:
+    def reductionOperations(self, nums: List[int]) -> int:
+        counter = Counter(nums)
+        arr = sorted((num, freq) for num, freq in counter.items())
+        ans = 0
+        for i in range(1, len(arr)):
+            ans += i * arr[i][1]
+        return ans
