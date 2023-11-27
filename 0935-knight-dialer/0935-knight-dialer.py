94dceb067d33a423a84976f780fd7305d0c7a46a
Trung Nguyen <32110646+trungnguyencs@users.noreply.github.com>
2023-11-27 16:23:35 -0700

    Time: 270 ms (91.91%), Space: 16.5 MB (67.98%) - LeetHub

---
+++ 0935-knight-dialer/0935-knight-dialer.py
@@ -0,0 +1,10 @@
+class Solution:
+    def knightDialer(self, N: int) -> int:
+        x1 = x2 = x3 = x4 = x5 = x6 = x7 = x8 = x9 = x0 = 1
+        for i in range(N - 1):
+            x1, x2, x3, x4, x5, x6, x7, x8, x9, x0 = \
+                x6 + x8, x7 + x9, x4 + x8, \
+                x3 + x9 + x0, 0, x1 + x7 + x0, \
+                x2 + x6, x1 + x3, x2 + x4, \
+                x4 + x6
+        return (x1 + x2 + x3 + x4 + x5 + x6 + x7 + x8 + x9 + x0) % (10**9 + 7)
