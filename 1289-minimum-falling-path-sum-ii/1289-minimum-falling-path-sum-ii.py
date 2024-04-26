1f61f52991616f12d2cf3703b08130c2520ebc0d
Trung Nguyen <32110646+trungnguyencs@users.noreply.github.com>
2024-04-25 21:17:55 -0700

    Time: 1478 ms (34.51%), Space: 20 MB (80.97%) - LeetHub

--- 1289-minimum-falling-path-sum-ii/1289-minimum-falling-path-sum-ii.py
+++ 1289-minimum-falling-path-sum-ii/1289-minimum-falling-path-sum-ii.py
@@ -1,12 +1,23 @@
 class Solution:
     def minFallingPathSum(self, grid: List[List[int]]) -> int:
         n = len(grid)
+        if n == 1: return grid[0][0]
         dp = grid[0]
         for r in range(1, n):
             newDp = [float('inf')]*n
-            for c1 in range(n):
-                for c2 in range(n):
-                    if c1 == c2: continue
-                    newDp[c1] = min(newDp[c1], grid[r][c1] + dp[c2])
+            for c in range(n):
+                # by finding Min & secondMin instead of a third level loop time is reduced from O(n^3) to O(n^2)
+                Min, minIdx, secondMin, secondMinIdx = self.findMinAndSecondMin(dp)
+                newDp[c] = min(newDp[c], grid[r][c] + (Min if c != minIdx else secondMin))
             dp = newDp
+        # time: O(n^2), space O(n). We can even further reduce space to O(1) by using just Min, minIdx, secondMin instead of dp array
         return min(dp)
+    
+    def findMinAndSecondMin(self, arr):
+        Min, minIdx, secondMin, secondMinIdx = float('inf'), -1, float('inf'), -1
+        for i, num in enumerate(arr):
+            if num <= Min:
+                Min, minIdx, secondMin, secondMinIdx = num, i, Min, minIdx
+            elif num < secondMin:
+                secondMin, secondMinIdx = num, i
+        return Min, minIdx, secondMin, secondMinIdx
