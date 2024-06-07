66643c061285dd4d9b2c44af34e2082fe5efb259
Trung Nguyen <32110646+trungnguyencs@users.noreply.github.com>
2024-06-06 17:12:16 -0700

    Time: 173 ms (34.23%), Space: 19 MB (5.50%) - LeetHub

--- 0846-hand-of-straights/0846-hand-of-straights.py
+++ 0846-hand-of-straights/0846-hand-of-straights.py
@@ -1,10 +1,18 @@
 class Solution:
     def isNStraightHand(self, hand: List[int], W: int) -> bool:
-        count = Counter(hand)
-        for y in sorted(count):
-            while count[y] > 0:
-                for k in range(y, y + W):
-                    count[k] -= 1
-                    if count[k] < 0:
-                        return False
+        if len(hand) % W != 0: return False
+        counter = Counter(hand)
+        heap = list(counter.items())
+        heapify(heap)
+        while heap:
+            if len(heap) < W: return False
+            tmp = []
+            for _ in range(W):
+                num, freq = heappop(heap)
+                if len(tmp) > 0 and num != tmp[-1][0] + 1: return False
+                tmp.append((num, freq - 1))
+            while tmp:
+                num, freq = tmp.pop()
+                if freq > 0:
+                    heappush(heap, (num, freq))
         return True
