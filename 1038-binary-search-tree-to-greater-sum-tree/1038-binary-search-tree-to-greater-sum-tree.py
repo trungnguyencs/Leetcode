ea4f223f881a39dab24b1cc539959aca87aab93e
Trung Nguyen <32110646+trungnguyencs@users.noreply.github.com>
2024-06-24 20:18:51 -0700

    Time: 44 ms (8.02%), Space: 16.6 MB (31.32%) - LeetHub

--- 1038-binary-search-tree-to-greater-sum-tree/1038-binary-search-tree-to-greater-sum-tree.py
+++ 1038-binary-search-tree-to-greater-sum-tree/1038-binary-search-tree-to-greater-sum-tree.py
@@ -5,18 +5,32 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def bstToGst(self, root: TreeNode) -> TreeNode:
-        self.total = self.postOrder(root)
-        self.curSum = 0
-        self.inOrder(root)
-        return root
+#     # Solution 1:
+#     def bstToGst(self, root: TreeNode) -> TreeNode:
+#         self.total = self.postOrder(root)
+#         self.curSum = 0
+#         self.inOrder(root)
+#         return root
         
-    def postOrder(self, root):
-        if not root: return 0
-        return root.val + self.postOrder(root.left) + self.postOrder(root.right)
+#     def postOrder(self, root):
+#         if not root: return 0
+#         return root.val + self.postOrder(root.left) + self.postOrder(root.right)
     
-    def inOrder(self, root):
+#     def inOrder(self, root):
+#         if not root: return
+#         self.inOrder(root.left)
+#         root.val, self.curSum = self.total - self.curSum, self.curSum + root.val
+#         self.inOrder(root.right)
+
+    # Solution 2:
+    def bstToGst(self, root: TreeNode) -> TreeNode:
+        self.curSum = 0
+        self.reversedInOrder(root)
+        return root
+    
+    def reversedInOrder(self, root):
         if not root: return
-        self.inOrder(root.left)
-        root.val, self.curSum = self.total - self.curSum, self.curSum + root.val
-        self.inOrder(root.right)
+        self.reversedInOrder(root.right)
+        self.curSum += root.val
+        root.val = self.curSum
+        self.reversedInOrder(root.left)
