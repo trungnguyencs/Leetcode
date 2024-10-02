b656fe7a4b9f4ddc46f58b9ad77f0f4d0b115553
Trung Nguyen <32110646+trungnguyencs@users.noreply.github.com>
2024-10-02 09:35:33 -0700

    Time: 49 ms (46.36%), Space: 16.9 MB (18.53%) - LeetHub

--- 1133-largest-unique-number/1133-largest-unique-number.py
+++ 1133-largest-unique-number/1133-largest-unique-number.py
@@ -1,5 +1,8 @@
 class Solution:
-    def largestUniqueNumber(self, A: List[int]) -> int:
-        counter = Counter(A)
-        uniq = [num for num in counter if counter[num] == 1]
-        return max(uniq) if uniq else -1
+    def largestUniqueNumber(self, nums: List[int]) -> int:
+        counter = Counter(nums)
+        ans = -1
+        for num, freq in counter.items():
+            if freq == 1 and num > ans:
+                ans = num
+        return ans
