e04ab448f8e88b654d92cb1c57a00d7973686450
Trung Nguyen <32110646+trungnguyencs@users.noreply.github.com>
2024-10-19 18:11:06 -0700

    Time: 0 ms (100.00%), Space: 16.4 MB (98.81%) - LeetHub

--- 1545-find-kth-bit-in-nth-binary-string/1545-find-kth-bit-in-nth-binary-string.py
+++ 1545-find-kth-bit-in-nth-binary-string/1545-find-kth-bit-in-nth-binary-string.py
@@ -1,10 +1,14 @@
 class Solution:
     def findKthBit(self, n: int, k: int) -> str:
+        # The first digit is always "0" regardless of n
+        # The middle digit is always 1
+        # If k < mid, one could reduce the problem to n-1 which is half the size
+        # Otherwise, focus on the other half by reversing the position and "0"/"1"
         length = 2**n - 1
         if k == 1: return '0'
         if k == length//2: return '1'
-        if k > length//2: return self.flip(self.findKthBit(n - 1, length + 1 - k))
-        return self.findKthBit(n - 1, k)
+        if k < length//2: return self.findKthBit(n - 1, k)
+        return self.flip(self.findKthBit(n - 1, length + 1 - k))
     
     def flip(self, bit):
         return '0' if bit == '1' else '1'
