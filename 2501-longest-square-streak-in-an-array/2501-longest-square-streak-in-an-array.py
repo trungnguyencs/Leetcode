7c4df43e40c0781caa9696bb3eaabacc14474ae3
Trung Nguyen <32110646+trungnguyencs@users.noreply.github.com>
2024-10-27 21:36:52 -0700

    Time: 199 ms (16.22%), Space: 34 MB (46.33%) - LeetHub

--- 2501-longest-square-streak-in-an-array/2501-longest-square-streak-in-an-array.py
+++ 2501-longest-square-streak-in-an-array/2501-longest-square-streak-in-an-array.py
@@ -1,16 +1,32 @@
 class Solution:
     def longestSquareStreak(self, nums: List[int]) -> int:
-        nums.sort()
+        # # O(nlogn)
+        # nums.sort()
+        # numSet = set(nums)
+        # visited = set()
+        # maxStreak = -1
+        # for num in nums:
+        #     if num in visited: continue
+        #     streak = 0
+        #     while num in numSet:
+        #         visited.add(num)
+        #         num = num**2
+        #         streak += 1
+        #     if streak > 1:
+        #         maxStreak = max(maxStreak, streak)
+        # return maxStreak
+        
+        # O(n): similar to 128. Longest Consecutive Sequence
         numSet = set(nums)
         visited = set()
         maxStreak = -1
         for num in nums:
-            if num in visited: continue
+            sqrt = int(num**0.5)
+            if sqrt in numSet and sqrt**2 == num:
+                continue
             streak = 0
             while num in numSet:
-                visited.add(num)
                 num = num**2
                 streak += 1
-            if streak > 1:
-                maxStreak = max(maxStreak, streak)
-        return maxStreak
+            maxStreak = max(maxStreak, streak)
+        return -1 if maxStreak == 1 else maxStreak
