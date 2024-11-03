df7043695fdc116fb33e7df65f2b1fe5ef54f42b
Trung Nguyen <32110646+trungnguyencs@users.noreply.github.com>
2024-11-03 13:53:23 -0800

    Time: 795 ms (82.27%), Space: 37 MB (51.25%) - LeetHub

---
+++ 2955-number-of-same-end-substrings/2955-number-of-same-end-substrings.py
@@ -0,0 +1,12 @@
+class Solution:
+    def sameEndSubstringCount(self, s: str, queries: List[List[int]]) -> List[int]:
+        counter = [0]*26
+        dic = {-1: counter[:]}
+        for i, ch in enumerate(s):
+            counter[ord(ch) - ord('a')] += 1
+            dic[i] = counter[:]
+        return [self.sameEndSubstringCountHelper(s, dic[l - 1], dic[r]) for l, r in queries]
+        
+    def sameEndSubstringCountHelper(self, s, counterL, counterR):
+        counterDiff = [b - a for a, b in zip(counterL, counterR)]
+        return sum([n*(n + 1)//2 for n in counterDiff])
