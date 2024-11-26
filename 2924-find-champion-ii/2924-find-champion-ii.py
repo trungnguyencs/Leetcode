0a980cdca22b4a36e08c87a2449ed1ba6d959807
Trung Nguyen <32110646+trungnguyencs@users.noreply.github.com>
2024-11-25 18:35:17 -0800

    Time: 7 ms (68.21%), Space: 17.9 MB (44.02%) - LeetHub

--- 2924-find-champion-ii/2924-find-champion-ii.py
+++ 2924-find-champion-ii/2924-find-champion-ii.py
@@ -1,22 +1,9 @@
 class Solution:
     def findChampion(self, n: int, edges: List[List[int]]) -> int:
-        G, ind = self.buildGraph(edges)
-        q = deque([x for x in range(n) if ind[x] == 0])
-        roots = []
-        while q:
-            cur = q.popleft()
-            if not G[cur]: #cur node has no parent
-                roots.append(cur)
-            for parent in G[cur]:
-                ind[parent] -= 1
-                if ind[parent] == 0:
-                    q.append(parent)
-        return roots[0] if len(roots) == 1 else -1
-    
-    def buildGraph(self, edges):
-        G = defaultdict(list)
-        ind = defaultdict(int)
+        # topological sort works
+        # better solution is finding the nodes where outdegree == 0
+        outdegree = defaultdict(int)
         for s, e in edges:
-            G[e].append(s)
-            ind[s] += 1
-        return G, ind
+            outdegree[e] += 1
+        roots = [x for x in range(n) if outdegree[x] == 0]
+        return roots[0] if len(roots) == 1 else -1    
