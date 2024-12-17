e2d688af2e6ded6e51fcfac9bdf757b35eab3e43
Trung Nguyen <32110646+trungnguyencs@users.noreply.github.com>
2024-12-17 14:48:45 -0800

    Time: 3 ms (31.88%), Space: 17.7 MB (9.03%) - LeetHub

--- 0065-valid-number/0065-valid-number.py
+++ 0065-valid-number/0065-valid-number.py
@@ -1,6 +1,7 @@
 import re
 class Solution:
     def isNumber(self, s: str) -> bool:
+        #subpattern handles everything before [eE], "1." and ".1" are both True, but "." is False
         subpattern = f"[+-]?(([0-9]+\.?[0-9]*)|([0-9]*\.?[0-9]+))"
         pattern = f"^{subpattern}([eE][+-]?[0-9]+)?$"
         return bool(re.match(pattern, s.strip()))
