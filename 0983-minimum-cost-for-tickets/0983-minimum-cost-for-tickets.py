4770b96fa2509dd39f217750d6ebc87363fd55f8
Trung Nguyen <32110646+trungnguyencs@users.noreply.github.com>
2024-12-30 19:13:00 -0800

    Time: 7 ms (44.54%), Space: 17.7 MB (28.23%) - LeetHub

--- 0983-minimum-cost-for-tickets/0983-minimum-cost-for-tickets.py
+++ 0983-minimum-cost-for-tickets/0983-minimum-cost-for-tickets.py
@@ -1,9 +1,12 @@
 class Solution:
     def mincostTickets(self, days: List[int], costs: List[int]) -> int:
+        dayPass, weekPass, monthPass = costs
         dp = [0]*(days[-1] + 1)
-        for d in range(1, days[-1] + 1):
-            if d not in days:
-                dp[d] = dp[d-1]
+        for i in range(1, days[-1] + 1):
+            if i not in days:
+                dp[i] = dp[i-1]
             else:
-                dp[d] = min(dp[d-1] + costs[0], dp[max(0, d-7)] + costs[1], dp[max(0, d-30)] + costs[2])
+                dp[i] = min(dp[i-1] + dayPass,
+                            dp[max(0, i-7)] + weekPass,
+                            dp[max(0, i-30)] + monthPass)
         return dp[-1]
