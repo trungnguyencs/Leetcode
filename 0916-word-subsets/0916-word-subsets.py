3ab8dc1a360e8fafaf8e610e8416b045346ee365
Trung Nguyen <32110646+trungnguyencs@users.noreply.github.com>
2025-01-09 16:24:02 -0800

    Time: 404 ms (55.38%), Space: 21.3 MB (21.91%) - LeetHub

--- 0916-word-subsets/0916-word-subsets.py
+++ 0916-word-subsets/0916-word-subsets.py
@@ -1,12 +1,13 @@
 class Solution:
-    def wordSubsets(self, A: List[str], B: List[str]) -> List[str]:
+    def wordSubsets(self, words1: List[str], words2: List[str]) -> List[str]:
+        masterCounter = Counter()
+        for word in words2:
+            counter = Counter(word)
+            for ch, freq in counter.items():
+                masterCounter[ch] = max(masterCounter[ch], freq)
         ans = []
-        counterB = Counter()
-        for word in B:
-            tmpCounter = Counter(word)
-            for c in tmpCounter:
-                counterB[c] = max(counterB[c], tmpCounter[c])
-        for word in A:
-            tmpCounter = Counter(word)
-            if all([counterB[c] <= tmpCounter[c] for c in counterB]): ans.append(word)
+        for word in words1:
+            counter = Counter(word)
+            if all(freq <= counter[ch] for ch, freq in masterCounter.items()):
+                ans.append(word)
         return ans
