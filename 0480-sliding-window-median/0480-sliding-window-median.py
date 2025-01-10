ad25b97f9554521210adb50b3252ef85272c10e3
Trung Nguyen <32110646+trungnguyencs@users.noreply.github.com>
2025-01-10 10:27:35 -0800

    Time: 268 ms (30.54%), Space: 30.8 MB (80.43%) - LeetHub

--- 0480-sliding-window-median/0480-sliding-window-median.py
+++ 0480-sliding-window-median/0480-sliding-window-median.py
@@ -1,18 +1,34 @@
-from bisect import bisect
-
 class Solution:
+
+    # #Solution 1: Maintain a sorted sliding window (using regular list)
+    # #Runtime: O(klogk + nk) = O(nk)
+    # from bisect import bisect
+    # def medianSlidingWindow(self, nums: List[int], k: int) -> List[float]:
+    #     window = []
+    #     for i in range(k):
+    #         window.append(nums[i])
+    #     window.sort()
+    #     ans = [self.getMedianFromSortedArray(window)]
+    #     for i in range(k, len(nums)):
+    #         lastNum = nums[i-k]
+    #         lastNumIndex = bisect(window, lastNum) - 1
+    #         del window[lastNumIndex]
+    #         indexToInsert = bisect(window, nums[i])
+    #         window.insert(indexToInsert, nums[i])
+    #         ans.append(self.getMedianFromSortedArray(window))
+    #     return ans
+
+    #Solution 2: Maintain a sorted sliding window (using SortedList)
+    #Runtime: O(nlogk)
+    from sortedcontainers import SortedList
     def medianSlidingWindow(self, nums: List[int], k: int) -> List[float]:
-        window = []
+        window = SortedList()
         for i in range(k):
-            window.append(nums[i])
-        window.sort()
+            window.add(nums[i])
         ans = [self.getMedianFromSortedArray(window)]
         for i in range(k, len(nums)):
-            lastNum = nums[i-k]
-            lastNumIndex = bisect(window, lastNum) - 1
-            del window[lastNumIndex]
-            indexToInsert = bisect(window, nums[i])
-            window.insert(indexToInsert, nums[i])
+            window.remove(nums[i-k])
+            window.add(nums[i])
             ans.append(self.getMedianFromSortedArray(window))
         return ans
 
