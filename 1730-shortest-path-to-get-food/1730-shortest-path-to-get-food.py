aa3c74597f0f717ff14955f0ccbe6ff7e1f01d85
Trung Nguyen <32110646+trungnguyencs@users.noreply.github.com>
2025-01-14 16:34:12 -0800

    Time: 100 ms (30.3%), Space: 26.9 MB (18.94%) - LeetHub

--- 1730-shortest-path-to-get-food/1730-shortest-path-to-get-food.py
+++ 1730-shortest-path-to-get-food/1730-shortest-path-to-get-food.py
@@ -1,18 +1,22 @@
 class Solution:
     def getFood(self, grid: List[List[str]]) -> int:
-        start = self.findStartPoint(grid)
-        visited = {start}
-        q = deque([(start, 0)])
+        r0, c0 = self.findStartLocation(grid)
+        visited = set([(r0, c0)])
+        q = deque([(r0, c0, 0)])
         while q:
-            (r, c), d = q.popleft()
-            for nr, nc in [(r-1, c), (r+1, c), (r, c-1), (r, c+1)]:
-                if 0 <= nr < len(grid) and 0 <= nc < len(grid[0]) and (nr, nc) not in visited and grid[nr][nc] in 'O#':
-                    if grid[nr][nc] == '#': return d + 1
-                    visited.add((nr, nc))
-                    q.append([(nr, nc), d + 1])
+            r, c, d = q.popleft()
+            neighs = [(r-1, c), (r+1, c), (r, c-1), (r, c+1)]
+            for nr, nc in neighs:
+                if 0 <= nr < len(grid) and 0 <= nc < len(grid[0]):
+                    if grid[nr][nc] == '#':
+                        return d + 1
+                    if grid[nr][nc] == 'O' and (nr, nc) not in visited:
+                        visited.add((nr, nc))
+                        q.append((nr, nc, d + 1))
         return -1
-    
-    def findStartPoint(self, grid):
+
+    def findStartLocation(self, grid):
         for r in range(len(grid)):
             for c in range(len(grid[0])):
-                if grid[r][c] == '*': return (r, c)
+                if grid[r][c] == '*':
+                    return (r, c)
