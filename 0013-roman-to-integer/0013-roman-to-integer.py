0471c73081d9183e762b73d80a1b4f5b47dc977a
Trung Nguyen <32110646+trungnguyencs@users.noreply.github.com>
2025-01-16 17:03:42 -0800

    Time: 7 ms (43.14%), Space: 17.6 MB (47.11%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.py
+++ 0013-roman-to-integer/0013-roman-to-integer.py
@@ -1,8 +1,13 @@
 class Solution:
     def romanToInt(self, s: str) -> int:
-        ans = 0
         dic = {'M': 1000, 'D': 500, 'C': 100, 'L': 50, 'X': 10, 'V': 5, 'I': 1}
-        for ch in reversed(s):
-            val = dic[ch]
-            ans += val if ans < 5 * val else -val
+        #Go from back to front
+        #If current char is smaller than next char, substract it. Else, add it
+        ans = dic[s[-1]]
+        for i in range(len(s) - 2, -1, -1):
+            curVal, nextVal = dic[s[i]], dic[s[i+1]]
+            if curVal < nextVal:
+                ans -= curVal
+            else:
+                ans += curVal
         return ans
