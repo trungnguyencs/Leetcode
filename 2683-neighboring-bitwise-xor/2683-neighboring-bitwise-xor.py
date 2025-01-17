17bf0106ee4123a4b60d361930fdf24cce99fda8
Trung Nguyen <32110646+trungnguyencs@users.noreply.github.com>
2025-01-16 18:26:23 -0800

    Time: 63 ms (42.34%), Space: 22.3 MB (48.9%) - LeetHub

--- 2683-neighboring-bitwise-xor/2683-neighboring-bitwise-xor.py
+++ 2683-neighboring-bitwise-xor/2683-neighboring-bitwise-xor.py
@@ -1,10 +1,13 @@
 class Solution:
     def doesValidArrayExist(self, derived: List[int]) -> bool:
+        # original[i] ^ original[i + 1] = derived[i]
+        # => original[i] ^ original[i] ^ original[i + 1] = original[i] ^ derived[i]
+        # => original[i + 1] = original[i] ^ derived[i]
+        # start with original[0] in [0, 1], and start xor with derived. Check if in the end it returns to the original value
         return self.helper(derived, 0) or self.helper(derived, 1)
 
     def helper(self, derived, firstBit):
         curBit = firstBit
         for b in derived:
-            next = b^curBit
-            curBit = next
-        return next == firstBit
+            curBit ^= b
+        return curBit == firstBit
