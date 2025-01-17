3d64af344dba1158590c69add1a0cc0e42ecee44
Trung Nguyen <32110646+trungnguyencs@users.noreply.github.com>
2025-01-16 17:10:34 -0800

    Time: 5 ms (53.98%), Space: 17.6 MB (47.11%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.py
+++ 0013-roman-to-integer/0013-roman-to-integer.py
@@ -1,13 +1,10 @@
 class Solution:
     def romanToInt(self, s: str) -> int:
         dic = {'M': 1000, 'D': 500, 'C': 100, 'L': 50, 'X': 10, 'V': 5, 'I': 1}
-        #Go from back to front
         #If current char is smaller than next char, substract it. Else, add it
-        ans = dic[s[-1]]
-        for i in range(len(s) - 2, -1, -1):
+        ans = 0
+        for i in range(len(s) - 1):
             curVal, nextVal = dic[s[i]], dic[s[i+1]]
-            if curVal < nextVal:
-                ans -= curVal
-            else:
-                ans += curVal
+            ans += curVal if curVal >= nextVal else -curVal
+        ans += dic[s[-1]]
         return ans
