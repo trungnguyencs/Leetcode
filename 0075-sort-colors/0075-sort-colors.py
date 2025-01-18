9ccb0c197de338b8fa6c1b753489985331edcb92
Trung Nguyen <32110646+trungnguyencs@users.noreply.github.com>
2025-01-17 17:44:45 -0800

    Time: 0 ms (100%), Space: 18 MB (6.72%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.py
+++ 0075-sort-colors/0075-sort-colors.py
@@ -1,16 +1,31 @@
 class Solution:
     def sortColors(self, nums: List[int]) -> None:
         """
-        Do not return anything, modify nums in-place instead.
+        #1st approach - Loi giai sai:
+        left, right = 0, len(nums) - 1
+        for mid in range(len(nums)):
+            if nums[mid] == 0:
+                nums[mid], nums[left] = nums[left], nums[mid]
+                left += 1
+            elif nums[mid] == 2:
+                #sai cho nay vi ta khong biet nums[right] la gi
+                #neu la 2 thi so 2 nay se bi swap ra giua, truoc cac so 1 sau nay
+                #neu la 0 thi so 0 nay co the dung sau cac so 1 truoc do
+                #nhu vay neu nums[mid] == 2 thi ko duoc move mid
+                nums[mid], nums[right] = nums[right], nums[mid]
+                right -= 1
+        return nums
         """
-        l, m, r = 0, 0, len(nums) - 1
-        while m <= r:
-            if nums[m] == 0:
-                nums[m], nums[l] = nums[l], nums[m]
-                l += 1
-                m += 1
-            elif nums[m] == 2:
-                nums[m], nums[r] = nums[r], nums[m]
-                r -= 1
+        #Loi giai dung:
+        left, mid, right = 0, 0, len(nums) - 1
+        while mid <= right:
+            if nums[mid] == 0:
+                nums[mid], nums[left] = nums[left], nums[mid]
+                left += 1
+                mid += 1
+            elif nums[mid] == 1:
+                mid += 1
             else:
-                m += 1
+                nums[mid], nums[right] = nums[right], nums[mid]
+                right -= 1
+        return nums
