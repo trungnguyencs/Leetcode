7a850c3e665301e867b8cd444bcd95b226e56285
Trung Nguyen <32110646+trungnguyencs@users.noreply.github.com>
2025-01-17 16:10:25 -0800

    Time: 35 ms (79.13%), Space: 17.9 MB (12.64%) - LeetHub

--- 0535-encode-and-decode-tinyurl/0535-encode-and-decode-tinyurl.py
+++ 0535-encode-and-decode-tinyurl/0535-encode-and-decode-tinyurl.py
@@ -2,22 +2,34 @@ class Codec:
 
     def __init__(self):
         self.alphabet = string.ascii_letters + string.digits
-        self.url2Code, self.code2Url = {}, {}
-        
+        self.baseUrl = 'tinyurl.com/'
+        self.urlToCode = {}
+        self.codeToUrl = {}
+
     def encode(self, longUrl: str) -> str:
         """Encodes a URL to a shortened URL.
         """
-        while longUrl not in self.url2Code:
-            code = ''.join([choice(self.alphabet) for _ in range(6)])
-            if code not in self.code2Url:
-                self.code2Url[code] = longUrl
-                self.url2Code[longUrl] = code
-        return "http://tinyurl.com/" + self.url2Code[longUrl]
-        
+        if longUrl in self.urlToCode:
+            code = self.urlToCode[longUrl]
+        else:
+            code = self._generateRandomCode()
+            while code in self.codeToUrl:
+                code = self._generateRandomCode()
+            self.urlToCode[longUrl] = code
+            self.codeToUrl[code] = longUrl
+        return self.baseUrl + code
+
     def decode(self, shortUrl: str) -> str:
         """Decodes a shortened URL to its original URL.
         """
-        return self.code2Url[shortUrl[-6:]]
+        code = shortUrl[-6:]
+        return self.codeToUrl[code]
+        
+    def _generateRandomCode(self):
+        code = ''
+        for _ in range(6):
+            code += random.choice(self.alphabet)
+        return code
 
 # Your Codec object will be instantiated and called as such:
 # codec = Codec()
