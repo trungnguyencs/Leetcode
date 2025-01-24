10835d807457ed44deaf1b834dcac842e702c83c
Trung Nguyen <32110646+trungnguyencs@users.noreply.github.com>
2025-01-23 19:38:42 -0800

    Time: 100 ms (12.35%), Space: 26 MB (5.04%) - LeetHub

--- 0802-find-eventual-safe-states/0802-find-eventual-safe-states.py
+++ 0802-find-eventual-safe-states/0802-find-eventual-safe-states.py
@@ -1,20 +1,23 @@
 class Solution:
-    def eventualSafeNodes(self, parentToChildren: List[List[int]]) -> List[int]:
-        # keep removing all nodes with indegree == 0
-        # in the end all safe nodes will soon or later have indegrees == 0
-        childtoParents = defaultdict(list)
-        ind = defaultdict(int)
-        for parent, children in enumerate(parentToChildren):
-            ind[parent] = len(children)
-            for child in children:
-                childtoParents[child].append(parent)
-        q = deque([node for node in ind if ind[node] == 0])
-        ans = list(q)
+    def eventualSafeNodes(self, graph: List[List[int]]) -> List[int]:
+        #use Topological sort
+        ind, parents = self.buildGraph(graph)
+        q = deque([node for node in range(len(graph)) if ind[node] == 0])
+        ans = []
         while q:
-            child = q.popleft()
-            for parent in childtoParents[child]:
+            cur = q.popleft()
+            ans.append(cur)
+            for parent in parents[cur]:
                 ind[parent] -= 1
                 if ind[parent] == 0:
                     q.append(parent)
-                    ans.append(parent)
         return sorted(ans)
+
+    def buildGraph(self, graph):
+        ind = defaultdict(int)
+        parents = defaultdict(set)
+        for parent, childrens in enumerate(graph):
+            for child in childrens:
+                parents[child].add(parent)
+                ind[parent] += 1
+        return ind, parents
