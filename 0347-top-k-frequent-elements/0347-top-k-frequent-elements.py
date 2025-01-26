f90a9b1edb76b8dc955204f2ee384f752ec923b1
Trung Nguyen <32110646+trungnguyencs@users.noreply.github.com>
2025-01-26 03:32:41 -0800

    Time: 1 ms (95.05%), Space: 21.4 MB (36.19%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -5,6 +5,6 @@ class Solution:
         for num, freq in counter.items():
             if len(heap) < k:
                 heappush(heap, (freq, num))
-            elif freq > heap[0][0]:
+            elif freq >= heap[0][0]:
                 heapreplace(heap, (freq, num))
         return [num for _, num in heap]
