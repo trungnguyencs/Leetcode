3cfb54200bc588ab5edd86d6c7a4b25fc65d0e50
Trung Nguyen <32110646+trungnguyencs@users.noreply.github.com>
2025-01-31 16:19:18 -0800

    Time: 1077 ms (47.27%), Space: 27.9 MB (74.17%) - LeetHub

--- 0827-making-a-large-island/0827-making-a-large-island.py
+++ 0827-making-a-large-island/0827-making-a-large-island.py
@@ -1,32 +1,37 @@
 class Solution:
     def largestIsland(self, grid: List[List[int]]) -> int:
+        #color each island (starting from 2) and store its area in a dict
+        #for each 0, sum up all its neighbor islands of different colors
         dic = {}
         color = 2
         for r in range(len(grid)):
             for c in range(len(grid[0])):
                 if grid[r][c] == 1:
-                    dic[color] = self.getAreaOfIsland(grid, r, c, color)
+                    dic[color] = self.colorAndGetArea(grid, r, c, color)
                     color += 1
-        maxArea = max(dic.values()) if dic else 0 # in case of all ones -> not change any 0 to 1
+        maxArea = max(dic.values()) if dic else 0 #this is in case the grid has all 1s or all 0s
         for r in range(len(grid)):
             for c in range(len(grid[0])):
-                if grid[r][c] == 0:
-                    maxArea = max(maxArea, self.findCombinedArea(grid, r, c, dic))
+                if grid[r][c] != 0: continue
+                maxArea = max(maxArea, self.findCombinedArea(grid, r, c, dic))
         return maxArea
-        
-    def getAreaOfIsland(self, grid, r, c, color):
+
+    def colorAndGetArea(self, grid, r, c, color):
         grid[r][c] = color
         area = 1
-        for nr, nc in [(r-1, c), (r+1, c), (r, c-1), (r, c+1)]:
+        neighs = [(r-1, c), (r+1, c), (r, c-1), (r, c+1)]
+        for nr, nc in neighs:
             if 0 <= nr < len(grid) and 0 <= nc < len(grid[0]) and grid[nr][nc] == 1:
-                area += self.getAreaOfIsland(grid, nr, nc, color)
+                area += self.colorAndGetArea(grid, nr, nc, color)
         return area
-            
+
     def findCombinedArea(self, grid, r, c, dic):
         visited = {0}
         combinedArea = 1
-        for nr, nc in [(r-1, c), (r+1, c), (r, c-1), (r, c+1)]:
+        neighs = [(r-1, c), (r+1, c), (r, c-1), (r, c+1)]
+        for nr, nc in neighs:
             if 0 <= nr < len(grid) and 0 <= nc < len(grid[0]) and grid[nr][nc] not in visited:
-                combinedArea += dic[grid[nr][nc]]
-                visited.add(grid[nr][nc])
+                color = grid[nr][nc]
+                combinedArea += dic[color]
+                visited.add(color)
         return combinedArea
