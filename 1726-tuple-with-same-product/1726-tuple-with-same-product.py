c29cb2b8e8c4680a53b98f3942c9d8020d41daa6
Trung Nguyen <32110646+trungnguyencs@users.noreply.github.com>
2025-02-05 23:09:51 -0800

    Time: 599 ms (14.69%), Space: 46.3 MB (65.4%) - LeetHub

--- 1726-tuple-with-same-product/1726-tuple-with-same-product.py
+++ 1726-tuple-with-same-product/1726-tuple-with-same-product.py
@@ -1,8 +1,10 @@
 class Solution:
     def tupleSameProduct(self, nums: List[int]) -> int:
         counter = Counter()
+        ans = 0
         for i in range(len(nums)):
             for j in range(i + 1, len(nums)):
-                counter[nums[i] * nums[j]] += 1
-        # Choose 2 from freq pairs. Each pair has 8 tuple arrangements
-        return 8 * sum(math.comb(freq, 2) for freq in counter.values())
+                prod = nums[i] * nums[j]
+                ans += 8 * counter[prod] #Each pair has 8 tuple arrangements
+                counter[prod] += 1
+        return ans
