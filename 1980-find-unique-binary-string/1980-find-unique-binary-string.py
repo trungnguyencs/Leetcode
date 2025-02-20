5ea80aa772c2d1e197dafadbfe023b66affb9390
Trung Nguyen <32110646+trungnguyencs@users.noreply.github.com>
2025-02-19 20:36:06 -0800

    Time: 0 ms (100%), Space: 17.9 MB (52.45%) - LeetHub

--- 1980-find-unique-binary-string/1980-find-unique-binary-string.py
+++ 1980-find-unique-binary-string/1980-find-unique-binary-string.py
@@ -1,8 +1,16 @@
 class Solution:
     def findDifferentBinaryString(self, nums: List[str]) -> str:
-        # brute force solution: convert to number and find a number between [0, 2**n - 1] not in the input set
-        # smart solution: ith bit of ans is the opposite of ith bit of ith string -> ans differs from each string by at least 1 bit
-        ans = []
-        for i, s in enumerate(nums):
-            ans.append('1' if s[i] == '0' else '0')
-        return ''.join(ans)
+        n = len(nums[0])
+        nums = set(nums)
+        return self.backtrack(['']*n, 0, nums, n)
+
+    def backtrack(self, arr, i, nums, n):
+        if i == n:
+            s = ''.join(arr)
+            if s not in nums:
+                return s
+            return None
+        for d in '01':
+            arr[i] = d
+            s = self.backtrack(arr, i + 1, nums, n)
+            if s: return s
