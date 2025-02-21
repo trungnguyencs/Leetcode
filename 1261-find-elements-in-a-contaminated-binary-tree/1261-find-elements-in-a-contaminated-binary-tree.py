5249617b5708f1a10ca88e8fbd669eb893233296
Trung Nguyen <32110646+trungnguyencs@users.noreply.github.com>
2025-02-20 17:19:23 -0800

    Time: 8 ms (53.21%), Space: 21.9 MB (80.75%) - LeetHub

--- 1261-find-elements-in-a-contaminated-binary-tree/1261-find-elements-in-a-contaminated-binary-tree.py
+++ 1261-find-elements-in-a-contaminated-binary-tree/1261-find-elements-in-a-contaminated-binary-tree.py
@@ -7,15 +7,28 @@
 class FindElements:
 
     def __init__(self, root: Optional[TreeNode]):
-        self.numSet = set()
+        self.root = root
         self.dfs(root, 0)
 
+    #instead of using a hashset, this solution finds the path from root for each number
+    #notice that level-by-level of the tree will be in the form [[0], [1, 2], [3, 4, 5, 6]]
+    #if we aff 1 to all nums it will be [[1], [2, 3], [4, 5, 6, 7]]
+    #in binary will be [[1], [10, 11], [100, 101, 110, 111]]
+    #so 0 means turn left, 1 means turn right
     def find(self, target: int) -> bool:
-        return target in self.numSet
-    
+        cur = self.root
+        bits = bin(target + 1)[3:] #trim '0b1'
+        for b in bits:
+            if b == '0':
+                if not cur.left: return False
+                cur = cur.left
+            elif b == '1':
+                if not cur.right: return False
+                cur = cur.right
+        return True
+
     def dfs(self, root, val):
         root.val = val
-        self.numSet.add(val)
         if root.left:
             self.dfs(root.left, 2 * val + 1)
         if root.right:
