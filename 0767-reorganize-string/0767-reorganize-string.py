9f1f57877e9d2197f26f3ba5efb815d500b5be9e
Trung Nguyen <32110646+trungnguyencs@users.noreply.github.com>
2025-03-10 18:39:00 -0700

    Time: 0 ms (100%), Space: 17.9 MB (28.92%) - LeetHub

--- 0767-reorganize-string/0767-reorganize-string.py
+++ 0767-reorganize-string/0767-reorganize-string.py
@@ -1,13 +1,23 @@
 class Solution:
     def reorganizeString(self, s: str) -> str:
-        n = len(s)
-        ans = ['']*n
         counter = Counter(s)
-        counter = sorted([[count, num] for num, count in counter.items()], reverse=True)
-        if counter[0][0] > n//2 + (n % 2): return ''
-        i = 0
-        for count, c in counter:
-            for _ in range(count):
-                if i >= len(s): i = 1 #should happen once and only once
-                ans[i] = c; i += 2
+        heap = [(-freq, c) for c, freq in counter.items()]
+        heapify(heap)
+        ans = []
+        while heap:
+            if not ans or heap[0][1] != ans[-1]:
+                freq, c = heappop(heap)
+                ans.append(c)
+                freq += 1
+                if freq != 0:
+                    heappush(heap, (freq, c))
+            elif len(heap) < 2: return ''
+            else:
+                freq1, c1 = heappop(heap)
+                freq2, c2 = heappop(heap)
+                ans.append(c2)
+                freq2 += 1
+                heappush(heap, (freq1, c1))
+                if freq2 != 0:
+                    heappush(heap, (freq2, c2))
         return ''.join(ans)
