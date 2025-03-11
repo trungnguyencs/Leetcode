b250699ff012129cc46f5f17801532025bcce0d1
Trung Nguyen <32110646+trungnguyencs@users.noreply.github.com>
2025-03-10 18:46:21 -0700

    Time: 266 ms (37.94%), Space: 25.3 MB (28.01%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,16 +1,17 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        ans = 0
+        islands = 0
+        visited = set()
         for r in range(len(grid)):
             for c in range(len(grid[0])):
-                if grid[r][c] == '1':
-                    self.dfs(r, c, grid)
-                    ans += 1
-        return ans
-    
-    def dfs(self, r, c, grid):
-        grid[r][c] = '#'
-        neighs = ((r - 1, c), (r + 1, c), (r, c - 1), (r, c + 1))
+                if grid[r][c] == '1' and (r, c) not in visited:
+                    islands += 1
+                    self.dfs(grid, r, c, visited)
+        return islands
+
+    def dfs(self, grid, r, c, visited):
+        visited.add((r, c))
+        neighs = [(r-1, c), (r+1, c), (r, c-1), (r, c+1)]
         for nr, nc in neighs:
-            if 0 <= nr < len(grid) and 0 <= nc < len(grid[0]) and grid[nr][nc] == '1':
-                self.dfs(nr, nc, grid)
+            if 0 <= nr < len(grid) and 0 <= nc < len(grid[0]) and grid[nr][nc] == '1' and (nr, nc) not in visited:
+                self.dfs(grid, nr, nc, visited)
