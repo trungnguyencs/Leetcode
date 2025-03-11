cbce021139062cd17be81482e55c9c75a3678f12
Trung Nguyen <32110646+trungnguyencs@users.noreply.github.com>
2025-03-10 18:50:07 -0700

    Time: 19 ms (38.21%), Space: 22.7 MB (13.12%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -2,11 +2,11 @@ class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         dic = defaultdict(list)
         for s in strs:
-            dic[self.convert(s)].append(s)
-        return dic.values()
-    
-    def convert(self, s):
-        counter = [0]*26
-        for ch in s:
-            counter[ord(ch) - ord('a')] += 1
-        return tuple(counter)
+            dic[self.getFreqArr(s)].append(s)
+        return list(dic.values())
+
+    def getFreqArr(self, s):
+        arr = [0]*26
+        for c in s:
+            arr[ord(c) - ord('a')] += 1
+        return tuple(arr)
