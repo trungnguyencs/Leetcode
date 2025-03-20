4a22e02070a04f20c0c96821badf12976a138adc
Trung Nguyen <32110646+trungnguyencs@users.noreply.github.com>
2025-03-20 16:36:48 -0700

    Time: 67 ms (95.9%), Space: 17.9 MB (81.94%) - LeetHub

--- 0680-valid-palindrome-ii/0680-valid-palindrome-ii.py
+++ 0680-valid-palindrome-ii/0680-valid-palindrome-ii.py
@@ -3,12 +3,12 @@ class Solution:
         l, r = 0, len(s) - 1
         while l < r:
             if s[l] != s[r]:
-                return self.helper(s, l + 1, r) or self.helper(s, l, r - 1)
+                return self.isPalindromeBasic(s, l + 1, r) or self.isPalindromeBasic(s, l, r - 1)
             l += 1
             r -= 1
         return True
-    
-    def helper(self, s, l, r):
+
+    def isPalindromeBasic(self, s, l, r):
         while l < r:
             if s[l] != s[r]: return False
             l += 1
