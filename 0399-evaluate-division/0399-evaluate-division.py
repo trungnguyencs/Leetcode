4818527282385832d715f1e233a2fc8518137714
Trung Nguyen <32110646+trungnguyencs@users.noreply.github.com>
2025-03-20 15:58:44 -0700

    Time: 0 ms (100%), Space: 17.8 MB (74.73%) - LeetHub

--- 0399-evaluate-division/0399-evaluate-division.py
+++ 0399-evaluate-division/0399-evaluate-division.py
@@ -2,23 +2,23 @@ class Solution:
     def calcEquation(self, equations: List[List[str]], values: List[float], queries: List[List[str]]) -> List[float]:
         G = self.buildGraph(equations, values)
         return [self.answerQuery(s, e, G) for s, e in queries]
-
+        
     def buildGraph(self, equations, values):
         G = defaultdict(list)
-        for (s, e), val in zip(equations, values):
-            G[s].append((e, val))
-            G[e].append((s, 1/val))
+        for (s, e), ratio in zip(equations, values):
+            G[s].append((e, ratio))
+            G[e].append((s, 1.0/ratio))
         return G
-
+    
     def answerQuery(self, s, e, G):
-        if s not in G: return -1
-        q = deque([(s, 1)])
+        if s not in G or e not in G: return -1
+        q = deque([(s, 1.0)])
         visited = set([s])
         while q:
-            cur, curRate = q.popleft()
-            if cur == e: return curRate
-            for nxt, val in G[cur]:
-                if nxt not in visited:
-                    visited.add(nxt)
-                    q.append((nxt, curRate * val))
+            cur, rate = q.popleft()
+            if cur == e: return rate
+            for neigh, multiplier in G[cur]:
+                if neigh not in visited:
+                    q.append([neigh, rate * multiplier])
+                    visited.add(neigh)
         return -1
