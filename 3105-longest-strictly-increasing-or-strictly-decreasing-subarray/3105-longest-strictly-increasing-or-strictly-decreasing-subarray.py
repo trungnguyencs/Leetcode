505970015573fefd9c17ae024e90209683ecb2e8
Trung Nguyen <32110646+trungnguyencs@users.noreply.github.com>
2025-03-23 21:07:35 -0700

    Time: 4 ms (22.21%), Space: 17.8 MB (35.45%) - LeetHub

--- 3105-longest-strictly-increasing-or-strictly-decreasing-subarray/3105-longest-strictly-increasing-or-strictly-decreasing-subarray.py
+++ 3105-longest-strictly-increasing-or-strictly-decreasing-subarray/3105-longest-strictly-increasing-or-strictly-decreasing-subarray.py
@@ -1,16 +1,15 @@
 class Solution:
     def longestMonotonicSubarray(self, nums: List[int]) -> int:
-        if len(nums) == 1: return 1
-        lastDiff = 0
-        streak = maxStreak = 1
+        inc = dec = ans = 1
         for i in range(1, len(nums)):
             diff = nums[i] - nums[i-1]
-            if diff == 0:
-                streak = 1
-            elif diff * lastDiff < 0:
-                streak = 2
+            if diff > 0:
+                inc += 1
+                dec = 1
+            elif diff < 0:
+                dec += 1
+                inc = 1
             else:
-                streak += 1
-            maxStreak = max(maxStreak, streak)
-            lastDiff = diff
-        return maxStreak
+                inc = dec = 1
+            ans = max(ans, inc, dec)
+        return ans
