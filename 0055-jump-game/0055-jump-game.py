8f18dd807e888d5d1bddb21b04bd5434b38d64d1
Trung Nguyen <32110646+trungnguyencs@users.noreply.github.com>
2025-03-23 21:27:34 -0700

    Time: 43 ms (28.72%), Space: 18.4 MB (86.6%) - LeetHub

--- 0055-jump-game/0055-jump-game.py
+++ 0055-jump-game/0055-jump-game.py
@@ -1,9 +1,9 @@
 class Solution:
     def canJump(self, nums: List[int]) -> bool:
         #dp will take O(n^2)
-        #greedy O(n): going from the back of the array, at each index find out if it can be reached
-        cur = len(nums) - 1
-        for i in range(len(nums) - 2, -1, -1):
-            if i + nums[i] >= cur:
-                cur = i
-        return cur == 0
+        #greedy O(n): use a variable maxReach to find out if there's an index that cannot be reached
+        maxReach = 0
+        for i, num in enumerate(nums):
+            if maxReach < i: return False
+            maxReach = max(maxReach, i + num)
+        return True
