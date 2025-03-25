0ea6a5d6373ee83909b909cfd754893a3ff25c50
Trung Nguyen <32110646+trungnguyencs@users.noreply.github.com>
2025-03-25 13:18:40 -0700

    Time: 61 ms (14.88%), Space: 21 MB (55.54%) - LeetHub

--- 1650-lowest-common-ancestor-of-a-binary-tree-iii/1650-lowest-common-ancestor-of-a-binary-tree-iii.py
+++ 1650-lowest-common-ancestor-of-a-binary-tree-iii/1650-lowest-common-ancestor-of-a-binary-tree-iii.py
@@ -10,13 +10,20 @@ class Node:
 
 class Solution:
     def lowestCommonAncestor(self, p: 'Node', q: 'Node') -> 'Node':
-        seen = set([p, q])
-        while p or q:
-            if p:
-                p = p.parent
-                if p in seen: return p
-                seen.add(p)
-            if q:
-                q = q.parent
-                if q in seen: return q
-                seen.add(q)
+        # #O(n) space:
+        # seen = set([p, q])
+        # while p or q:
+        #     if p:
+        #         p = p.parent
+        #         if p in seen: return p
+        #         seen.add(p)
+        #     if q:
+        #         q = q.parent
+        #         if q in seen: return q
+        #         seen.add(q)
+        # O(1) space: similar to 160. Intersection of Two Linked Lists
+        pParent, qParent = p, q
+        while pParent != qParent:
+            pParent = pParent.parent if pParent.parent else q
+            qParent = qParent.parent if qParent.parent else p
+        return pParent
