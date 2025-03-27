fb6d658bad51fedeb791b8b39223f8abd4cd8bad
Trung Nguyen <32110646+trungnguyencs@users.noreply.github.com>
2025-03-26 21:46:11 -0700

    Time: 67 ms (53.47%), Space: 32 MB (80.2%) - LeetHub

--- 2780-minimum-index-of-a-valid-split/2780-minimum-index-of-a-valid-split.py
+++ 2780-minimum-index-of-a-valid-split/2780-minimum-index-of-a-valid-split.py
@@ -1,11 +1,33 @@
 class Solution:
     def minimumIndex(self, nums: List[int]) -> int:
-        counter = Counter(nums)
-        majorityNum = [num for num, freq in counter.items() if freq > len(nums)//2][0]
+        #O(n) space using hashmap is trivial
+        #Here we find the majority element using only O(1) space by implementing the voting algo from 169. Majority Element
+        majorityNum = self.findMajorityElement(nums)
+        majorityNumFreq = self.findMajorityElementFreq(nums, majorityNum)
         count = 0
         for i, num in enumerate(nums):
             if num == majorityNum:
                 count += 1
-            if count > (i + 1)//2 and counter[majorityNum] - count > (len(nums) - (i + 1))//2:
+            if count > (i + 1)//2 and majorityNumFreq - count > (len(nums) - (i + 1))//2:
                 return i
         return -1
+
+    def findMajorityElement(self, nums):
+        majorityNum = nums[0]
+        count = 1
+        for i in range(1, len(nums)):
+            if nums[i] == majorityNum:
+                count += 1
+            else:
+                count -= 1
+                if count == 0:
+                    majorityNum = nums[i]
+                    count = 1
+        return majorityNum
+
+    def findMajorityElementFreq(self, nums, majorityNum):
+        count = 0
+        for num in nums:
+            if num == majorityNum:
+                count += 1
+        return count
