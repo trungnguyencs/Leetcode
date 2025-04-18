bd2677e9873b4d1c4787bd6288e2ca960d770c7f
Trung Nguyen <32110646+trungnguyencs@users.noreply.github.com>
2025-04-18 14:22:26 -0700

    Time: 3 ms (97.04%), Space: 18 MB (20.58%) - LeetHub

--- 0038-count-and-say/0038-count-and-say.py
+++ 0038-count-and-say/0038-count-and-say.py
@@ -1,15 +1,13 @@
 class Solution:
     def countAndSay(self, n: int) -> str:
-        s = '1'
-        for _ in range(n - 1):
-            tmp = []
-            cur, count = s[0], 1
-            for i in range(1, len(s)):
-                if s[i] == cur:
-                    count += 1
-                else:
-                    tmp.extend([str(count), cur])
-                    cur, count = s[i], 1
-            tmp.extend([str(count), cur])
-            s = ''.join(tmp)
-        return s              
+        if n == 1: return '1'
+        s = self.countAndSay(n - 1) + '#'
+        cur, count = s[0], 0
+        ans = []
+        for ch in s:
+            if ch == cur:
+                count += 1
+            else:
+                ans.extend([str(count), cur])
+                cur, count = ch, 1
+        return ''.join(ans)
