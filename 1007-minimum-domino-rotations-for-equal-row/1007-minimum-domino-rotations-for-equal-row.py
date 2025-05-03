41cb4e0cf0a89995ab91e3788d1110b12a1bad3b
Trung Nguyen <32110646+trungnguyencs@users.noreply.github.com>
2025-05-02 17:48:14 -0700

    Time: 138 ms (6.54%), Space: 18.4 MB (66.24%) - LeetHub

--- 1007-minimum-domino-rotations-for-equal-row/1007-minimum-domino-rotations-for-equal-row.py
+++ 1007-minimum-domino-rotations-for-equal-row/1007-minimum-domino-rotations-for-equal-row.py
@@ -1,16 +1,11 @@
 class Solution:
     def minDominoRotations(self, tops: List[int], bottoms: List[int]) -> int:
-        ans = min(self.helper(tops, bottoms), self.helper(bottoms, tops))
-        return -1 if ans == float('inf') else ans
-        
-    def helper(self, tops, bottoms):
-        topCount = bottomCount = 0
-        base = tops[0]
-        for i in range(len(tops)):
-            if base not in [tops[i], bottoms[i]]:
-                return float('inf')
-            if tops[i] == base:
-                topCount += 1
-            if bottoms[i] == base:
-                bottomCount += 1
-        return min(len(tops) - topCount, len(tops) - bottomCount)
+        countA, countB, countSame = Counter(), Counter(), Counter()
+        for a, b in zip(tops, bottoms):
+            countA[a] += 1
+            countB[b] += 1
+            if a == b: countSame[a] += 1
+        for a in countA:
+            if countA[a] + countB[a] - countSame[a] == len(tops):
+                return min(countA[a] - countSame[a], countB[a] - countSame[a])
+        return -1
