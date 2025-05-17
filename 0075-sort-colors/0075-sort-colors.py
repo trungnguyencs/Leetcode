5ffb865f17a1cd86d9b03df30a34c4f1ab7fd8b4
Trung Nguyen <32110646+trungnguyencs@users.noreply.github.com>
2025-05-16 20:34:17 -0700

    Time: 0 ms (100%), Space: 17.6 MB (83.24%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.py
+++ 0075-sort-colors/0075-sort-colors.py
@@ -1,30 +1,16 @@
 class Solution:
     def sortColors(self, nums: List[int]) -> None:
         """
-        #1st approach - Loi giai sai:
-        left, right = 0, len(nums) - 1
-        for mid in range(len(nums)):
-            if nums[mid] == 0:
-                nums[mid], nums[left] = nums[left], nums[mid]
-                left += 1
-            elif nums[mid] == 2:
-                #sai cho nay vi ta khong biet nums[right] la gi
-                #neu la 2 thi so 2 nay se bi swap ra giua, truoc cac so 1 sau nay
-                #neu la 0 thi so 0 nay co the dung sau cac so 1 truoc do
-                #nhu vay neu nums[mid] == 2 thi ko duoc move mid
-                nums[mid], nums[right] = nums[right], nums[mid]
-                right -= 1
+        Do not return anything, modify nums in-place instead.
         """
-        #Loi giai dung:
-        left, mid, right = 0, 0, len(nums) - 1
-        while mid <= right:
-            if nums[mid] == 0:
-                nums[mid], nums[left] = nums[left], nums[mid]
-                left += 1
-                mid += 1
-            elif nums[mid] == 1:
-                mid += 1
+        l, m, r = 0, 0, len(nums) - 1
+        while m <= r:
+            if nums[m] == 0:
+                nums[l], nums[m] = nums[m], nums[l]
+                l += 1
+                m += 1
+            elif nums[m] == 2:
+                nums[r], nums[m] = nums[m], nums[r]
+                r -= 1 #khong duoc tang m vi sau khi swap m co the bang 0
             else:
-                nums[mid], nums[right] = nums[right], nums[mid]
-                right -= 1
-                #cho nay ko duoc mid += 1
+                m += 1
