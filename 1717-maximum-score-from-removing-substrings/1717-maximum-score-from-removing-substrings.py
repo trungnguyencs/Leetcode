3b8566ba7e5df872d5f2e1db52796a1eef730e4e
Trung Nguyen <32110646+trungnguyencs@users.noreply.github.com>
2025-07-23 16:48:04 -0700

    Time: 315 ms (24.67%), Space: 19.3 MB (66.23%) - LeetHub

--- 1717-maximum-score-from-removing-substrings/1717-maximum-score-from-removing-substrings.py
+++ 1717-maximum-score-from-removing-substrings/1717-maximum-score-from-removing-substrings.py
@@ -1,22 +1,24 @@
 class Solution:
     def maximumGain(self, s: str, x: int, y: int) -> int:
-        # greedy: assuming x > y so remove all 'ab' first then remove all 'ba'
+        #greedy: assuming x > y so we remove all 'ab' then remove all 'ba'
         ab, ba = ['a', 'b'], ['b', 'a']
-        if x < y:
-            x, y, ab, ba = y, x, ba, ab
-        ans = 0
-        stack = []
+        if x < y: return self.helper(s, y, x, ba, ab)
+        return self.helper(s, x, y, ab, ba)
+
+    def helper(self, s, x, y, ab, ba):
+        points = 0
+        stack1 = []
         for ch in s:
-            stack.append(ch)
-            if stack[-2:] == ab:
-                ans += x
-                stack.pop()
-                stack.pop()
-        stack2 = []
-        for ch in stack:
+            stack1.append(ch)
+            if stack1[-2:] == ab:
+                points += x
+                stack1.pop()
+                stack1.pop()
+        stack2 = [] 
+        for ch in stack1:
             stack2.append(ch)
             if stack2[-2:] == ba:
-                ans += y
+                points += y
                 stack2.pop()
                 stack2.pop()
-        return ans
+        return points
