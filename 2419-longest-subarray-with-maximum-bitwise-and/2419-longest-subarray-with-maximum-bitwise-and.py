bb726dfdaa740623582ee7435032cc1f8718eefe
Trung Nguyen <32110646+trungnguyencs@users.noreply.github.com>
2025-07-29 17:22:39 -0700

    Time: 79 ms (51.85%), Space: 21.3 MB (44.44%) - LeetHub

--- 2419-longest-subarray-with-maximum-bitwise-and/2419-longest-subarray-with-maximum-bitwise-and.py
+++ 2419-longest-subarray-with-maximum-bitwise-and/2419-longest-subarray-with-maximum-bitwise-and.py
@@ -1,11 +1,16 @@
-class Solution:
-    def longestSubarray(self, nums: List[int]) -> int:
+class Solution(object):
+    def longestSubarray(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        #maxBitWiseAND = max(nums)
         maxNum = max(nums)
         streak = maxStreak = 0
-        for r, num in enumerate(nums):
+        for num in nums:
             if num == maxNum:
                 streak += 1
                 maxStreak = max(maxStreak, streak)
             else:
                 streak = 0
-        return maxStreak       
+        return maxStreak
