5cf5d8de9d63a916d9026f573ca6eb28aed01119
Trung Nguyen <32110646+trungnguyencs@users.noreply.github.com>
2025-09-06 16:45:20 -0700

    Time: 51 ms (36.34%), Space: 18.5 MB (44.38%) - LeetHub

--- 1066-campus-bikes-ii/1066-campus-bikes-ii.py
+++ 1066-campus-bikes-ii/1066-campus-bikes-ii.py
@@ -1,7 +1,5 @@
 class Solution:
     def assignBikes(self, workers: List[List[int]], bikes: List[List[int]]) -> int:
-        def distance(w, b):
-            return abs(workers[w][0] - bikes[b][0]) + abs(workers[w][1] - bikes[b][1])
         heap = [[0, 0, 0]] # (total distance, worker index, bike-taken state)
         seen = set()
         while heap:
@@ -11,4 +9,7 @@ class Solution:
             seen.add((w, bikesTaken))
             for b in range(len(bikes)):
                 if (1 << b) & bikesTaken: continue # bike was already taken
-                heappush(heap, [d + distance(w, b), w + 1, bikesTaken | (1 << b)])
+                heappush(heap, [d + self.distance(workers[w], bikes[b]), w + 1, bikesTaken | (1 << b)])
+                
+    def distance(self, ptA, ptB):
+        return abs(ptA[0] - ptB[0]) + abs(ptA[1] - ptB[1])
