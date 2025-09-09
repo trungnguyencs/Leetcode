074a10ab7894b269a2504393f4de4b77b51e835d
Trung Nguyen <32110646+trungnguyencs@users.noreply.github.com>
2025-09-09 16:49:44 -0700

    Time: 1 ms (43.02%), Space: 17.9 MB (69.66%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.py
+++ 0022-generate-parentheses/0022-generate-parentheses.py
@@ -3,11 +3,12 @@ class Solution:
         self.ans = []
         self.backtrack([], 0, 0, n)
         return self.ans
-        
+
     def backtrack(self, arr, openCount, closeCount, n):
-        if openCount > n or openCount < closeCount: return
         if openCount == closeCount == n:
             self.ans.append(''.join(arr))
+            return
+        if closeCount > openCount or openCount > n:
             return
         self.backtrack(arr + ['('], openCount + 1, closeCount, n)
         self.backtrack(arr + [')'], openCount, closeCount + 1, n)
