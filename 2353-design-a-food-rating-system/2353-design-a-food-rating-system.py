e8a35ca9f2ae9936a15304c87f4ac76b3a85b7d8
Trung Nguyen <32110646+trungnguyencs@users.noreply.github.com>
2025-09-16 22:40:11 -0700

    Time: 230 ms (34.03%), Space: 49.6 MB (98.68%) - LeetHub

--- 2353-design-a-food-rating-system/2353-design-a-food-rating-system.py
+++ 2353-design-a-food-rating-system/2353-design-a-food-rating-system.py
@@ -3,20 +3,23 @@ from sortedcontainers import SortedList
 class FoodRatings:
 
     def __init__(self, foods: List[str], cuisines: List[str], ratings: List[int]):
-        self.dic = {}
-        self.data = defaultdict(SortedList)
-        for food, cuisine, rating in zip(foods, cuisines, ratings):
-            self.dic[food] = (cuisine, rating)
-            self.data[cuisine].add((-rating, food))
+        self.ratings = {}
+        self.categories = {}
+        self.sortedRatings = defaultdict(SortedList)
+        for food, category, rating in zip(foods, cuisines, ratings):
+            self.ratings[food] = rating
+            self.categories[food] = category
+            self.sortedRatings[category].add((-rating, food))
 
     def changeRating(self, food: str, newRating: int) -> None:
-        cuisine, oldRating = self.dic[food]
-        self.dic[food] = cuisine, newRating
-        self.data[cuisine].remove((-oldRating, food))
-        self.data[cuisine].add((-newRating, food))
+        oldRating = self.ratings[food]
+        category = self.categories[food]
+        self.ratings[food] = newRating
+        self.sortedRatings[category].remove((-oldRating, food))
+        self.sortedRatings[category].add((-newRating, food))
 
     def highestRated(self, cuisine: str) -> str:
-        return self.data[cuisine][0][1]
+        return self.sortedRatings[cuisine][0][1] 
 
 
 # Your FoodRatings object will be instantiated and called as such:
