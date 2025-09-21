08eba8548997aee8ce08385c4ea992acf755c30f
Trung Nguyen <32110646+trungnguyencs@users.noreply.github.com>
2025-09-21 16:55:05 -0700

    Time: 4 ms (85.88%), Space: 18.2 MB (35.49%) - LeetHub

--- 0605-can-place-flowers/0605-can-place-flowers.py
+++ 0605-can-place-flowers/0605-can-place-flowers.py
@@ -1,12 +1,13 @@
 class Solution:
     def canPlaceFlowers(self, flowerbed: List[int], n: int) -> bool:
+        arr = [0] + flowerbed + [0]
         canPlant = streak = 0
-        for num in [0] + flowerbed + [0]:
+        for num in arr:
             if num == 1:
                 streak = 0
             else:
                 streak += 1
                 if streak == 3:
-                    canPlant += 1
                     streak = 1
+                    canPlant += 1
         return canPlant >= n
