7899ba15502862835b8140f0bb032fe06658f5e6
Trung Nguyen <32110646+trungnguyencs@users.noreply.github.com>
2025-10-04 23:10:55 -0700

    Time: 27 ms (83.26%), Space: 19.1 MB (97.87%) - LeetHub

--- 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.py
+++ 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.py
@@ -1,17 +1,23 @@
 class Solution:
     def pacificAtlantic(self, grid: List[List[int]]) -> List[List[int]]:
-        visitedPacific, visitedAtlantic = set(), set()
-        for r in range(len(grid)):
-            self.dfs(grid, r, 0, visitedPacific)
-            self.dfs(grid, r, len(grid[0]) - 1, visitedAtlantic)
-        for c in range(len(grid[0])):
-            self.dfs(grid, 0, c, visitedPacific)
-            self.dfs(grid, len(grid) - 1, c, visitedAtlantic)
-        return visitedPacific.intersection(visitedAtlantic)
-        
-    def dfs(self, grid, r, c, visited):
-        visited.add((r, c))
-        neighs = [(r-1, c), (r+1, c), (r, c-1), (r, c+1)]
-        for nr, nc in neighs:
-            if 0 <= nr < len(grid) and 0 <= nc < len(grid[0]) and grid[nr][nc] >= grid[r][c] and (nr, nc) not in visited:
-                self.dfs(grid, nr, nc, visited)
+        R, C = len(grid), len(grid[0])
+        pacificQ = deque([(0, c) for c in range(C)] + [(r, 0) for r in range(R)])
+        atlanticQ = deque([(R-1, c) for c in range(C)] + [(r, C-1) for r in range(R)])
+        pacific = self.bfs(grid, pacificQ)
+        atlantic = self.bfs(grid, atlanticQ)
+        return list(pacific.intersection(atlantic))
+
+    def bfs(self, grid, q):
+        visited = set()
+        while q:
+            r, c = q.popleft()
+            if (r, c) in visited: continue
+            visited.add((r, c))
+            neighs = [(r-1, c), (r+1, c), (r, c-1), (r, c+1)]
+            for nr, nc in neighs:
+                if (nr, nc) not in visited and\
+                 0 <= nr < len(grid) and\
+                 0 <= nc < len(grid[0]) and\
+                 grid[nr][nc] >= grid[r][c]:
+                    q.append((nr, nc))
+        return visited
