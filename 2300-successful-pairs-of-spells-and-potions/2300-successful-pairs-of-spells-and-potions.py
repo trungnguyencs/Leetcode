8e35ceca895a2c47b10db8eaba527e437c4a48f2
Trung Nguyen <32110646+trungnguyencs@users.noreply.github.com>
2025-10-07 23:01:36 -0700

    Time: 234 ms (80.35%), Space: 42.8 MB (25.48%) - LeetHub

--- 2300-successful-pairs-of-spells-and-potions/2300-successful-pairs-of-spells-and-potions.py
+++ 2300-successful-pairs-of-spells-and-potions/2300-successful-pairs-of-spells-and-potions.py
@@ -1,5 +1,9 @@
 class Solution:
     def successfulPairs(self, spells: List[int], potions: List[int], success: int) -> List[int]:
-        n = len(potions)
         potions.sort()
-        return [n - bisect_left(potions, success/spell) for spell in spells]
+        ans = []
+        for spell in spells:
+            target = success/spell
+            i = bisect_left(potions, target)
+            ans.append(len(potions) - i)
+        return ans
