b3a0007fae979de576eb0803cd229912c4127599
Trung Nguyen <32110646+trungnguyencs@users.noreply.github.com>
2025-10-24 18:03:50 -0700

    Time: 0 ms (100%), Space: 17.9 MB (37.64%) - LeetHub

--- 1716-calculate-money-in-leetcode-bank/1716-calculate-money-in-leetcode-bank.py
+++ 1716-calculate-money-in-leetcode-bank/1716-calculate-money-in-leetcode-bank.py
@@ -1,7 +1,6 @@
 class Solution:
     def totalMoney(self, n: int) -> int:
-        perWeek = (7+1)*7//2
-        weekCount = n//7
-        remainDays = n % 7
-        remain = ((weekCount + 1) + (weekCount + remainDays))*remainDays//2
-        return perWeek*weekCount + 7*weekCount*(weekCount - 1)//2 + remain
+        perWeek = (7 + 1) * 7 // 2
+        weekCount, remainDays = divmod(n, 7)
+        remain = ((weekCount + 1) + (weekCount + remainDays)) * remainDays // 2
+        return perWeek * weekCount + 7 * weekCount * (weekCount - 1) // 2 + remain
