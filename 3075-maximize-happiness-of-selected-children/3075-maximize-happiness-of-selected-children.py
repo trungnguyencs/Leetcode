4f5a33a1445d9ef3ead127b6a66ce651c4801d71
Trung Nguyen <32110646+trungnguyencs@users.noreply.github.com>
2025-12-24 22:17:34 -0800

    Time: 1088 ms (5.68%), Space: 48.3 MB (11.35%) - LeetHub

--- 3075-maximize-happiness-of-selected-children/3075-maximize-happiness-of-selected-children.py
+++ 3075-maximize-happiness-of-selected-children/3075-maximize-happiness-of-selected-children.py
@@ -1,7 +1,11 @@
 class Solution:
     def maximumHappinessSum(self, happiness: List[int], k: int) -> int:
-        happiness.sort(reverse=True)
-        ans = 0
-        for i in range(k):
-            ans += max(0, happiness[i] - i)
+        arr = heapq.nlargest(k, happiness)
+        arr.sort(reverse=True)
+        ans = t = 0
+        for num in arr:
+            if num <= t:
+                break
+            ans += num - t
+            t += 1      
         return ans
