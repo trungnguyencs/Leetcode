f5a4a35a09e3032daa193f70e7a97ad805b1b846
Trung Nguyen <32110646+trungnguyencs@users.noreply.github.com>
2025-12-31 20:28:56 -0800

    Time: 0 ms (100%), Space: 17 MB (99.34%) - LeetHub

---
+++ 0066-plus-one/0066-plus-one.py
@@ -0,0 +1,9 @@
+class Solution:
+    def plusOne(self, digits: List[int]) -> List[int]:
+        carry = 1
+        i = len(digits)-1
+        while i >= 0 and carry:
+            digits[i] += 1
+            carry, digits[i] = divmod(digits[i], 10)
+            i -= 1
+        return [1] + digits if carry == 1 else digits
