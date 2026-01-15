4d8401381834605ba4f30b1c096d076245c69400
Trung Nguyen <32110646+trungnguyencs@users.noreply.github.com>
2026-01-14 18:25:32 -0800

    Time: 47 ms (34.45%), Space: 20 MB (19.82%) - LeetHub

--- 0505-the-maze-ii/0505-the-maze-ii.py
+++ 0505-the-maze-ii/0505-the-maze-ii.py
@@ -1,20 +1,23 @@
 class Solution:
     def shortestDistance(self, maze: List[List[int]], start: List[int], destination: List[int]) -> int:
-        minDistance = float('inf')
-        q = deque([start])
         visited = {tuple(start): 0}
-        while q:
-            r, c = q.popleft()
-            directions = [[-1, 0], [1, 0], [0, -1], [0, 1]]
-            for dr, dc in directions:
-                nr, nc, d = r, c, visited[(r, c)]
-                while 0 <= nr + dr < len(maze) and 0 <= nc + dc < len(maze[0]) and maze[nr+dr][nc+dc] == 0:
+        heap = [(0, start)]
+        ans = float('inf')
+        #heap isn't better than queue because there's a case
+        #where it's the shortest so far but the last segment is so long it becomes not shortest overall
+        #so we still need to pop till heap/queue is empty
+        while heap:
+            d, cur = heappop(heap)
+            if cur == tuple(destination):
+                ans = min(ans, d)
+            r, c = cur
+            for dr, dc in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
+                nr, nc, nd = r, c, d
+                while 0 <= nr + dr < len(maze) and 0 <= nc + dc < len(maze[0]) and maze[nr + dr][nc + dc] == 0:
                     nr += dr
                     nc += dc
-                    d += 1
-                if [nr, nc] == destination: minDistance = min(minDistance, d)
-                if d > minDistance: continue                  
-                if (nr, nc) not in visited or visited[(nr, nc)] > d:
-                    visited[(nr, nc)] = d
-                    q.append((nr, nc))
-        return -1 if minDistance == float('inf') else minDistance
+                    nd += 1
+                if (nr, nc) not in visited or visited[(nr, nc)] > nd:
+                    visited[(nr, nc)] = nd
+                    heappush(heap, (nd, (nr, nc)))
+        return ans if ans != float('inf') else -1
