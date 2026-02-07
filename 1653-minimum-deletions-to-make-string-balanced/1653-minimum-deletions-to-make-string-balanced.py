4d7cc22f0554ace5042006790f0de94ac82d742b
Trung Nguyen <32110646+trungnguyencs@users.noreply.github.com>
2026-02-08 02:36:28 +0700

    Time: 303 ms (53.24%), Space: 20.4 MB (38.92%) - LeetHub

--- 1653-minimum-deletions-to-make-string-balanced/1653-minimum-deletions-to-make-string-balanced.py
+++ 1653-minimum-deletions-to-make-string-balanced/1653-minimum-deletions-to-make-string-balanced.py
@@ -4,12 +4,11 @@ class Solution:
         a = b = 0
         ans = len(s)
         for i, ch in enumerate(s):
-            #delete all prior b and future a
+            #delete all prior b and future a. Current char should not be deleted
             if ch == 'a':
                 a += 1
                 rightA = aCount - a
                 ans = min(ans, b + rightA)
-                
             else:
                 ans = min(ans, b + aCount - a)
                 b += 1
