b1bcadd4c7a51b45a425be42a172f7742bcb57a2
Trung Nguyen <32110646+trungnguyencs@users.noreply.github.com>
2026-02-10 03:12:18 +0700

    Time: 35 ms (46.71%), Space: 25.2 MB (32.99%) - LeetHub

--- 1382-balance-a-binary-search-tree/1382-balance-a-binary-search-tree.py
+++ 1382-balance-a-binary-search-tree/1382-balance-a-binary-search-tree.py
@@ -5,21 +5,21 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def balanceBST(self, root: TreeNode) -> TreeNode:
-        self.sortedArr = []
-        self.inorder(root)
-        return self.sortedArrToBalancedBST(self.sortedArr, 0, len(self.sortedArr) - 1)
-        
-    def inorder(self, root):
+    def balanceBST(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        arr = []
+        self.inOrder(root, arr)
+        return self.createBalancedBst(arr, 0, len(arr) - 1)
+
+    def inOrder(self, root, arr):
         if not root: return
-        self.inorder(root.left)
-        self.sortedArr.append(root)
-        self.inorder(root.right)
-        
-    def sortedArrToBalancedBST(self, arr, l, r):
+        self.inOrder(root.left, arr)
+        arr.append(root.val)
+        self.inOrder(root.right, arr)
+
+    def createBalancedBst(self, arr, l, r):
         if l > r: return None
         m = (l + r)//2
-        root = arr[m]
-        root.left = self.sortedArrToBalancedBST(arr, l, m - 1)
-        root.right = self.sortedArrToBalancedBST(arr, m + 1, r)
+        root = TreeNode(arr[m])
+        root.left = self.createBalancedBst(arr, l, m - 1)
+        root.right = self.createBalancedBst(arr, m + 1, r)
         return root
