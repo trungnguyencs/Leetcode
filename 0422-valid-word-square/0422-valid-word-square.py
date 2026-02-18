796f26b64c7af04f2a2c70275138e3de1ef250d9
Trung Nguyen <32110646+trungnguyencs@users.noreply.github.com>
2026-02-18 22:51:07 +0700

    Time: 23 ms (27.58%), Space: 20 MB (30.18%) - LeetHub

--- 0422-valid-word-square/0422-valid-word-square.py
+++ 0422-valid-word-square/0422-valid-word-square.py
@@ -1,8 +1,7 @@
-class Solution:
-    def validWordSquare(self, words: List[str]) -> bool:
-        if len(words) != len(max(words, key=len)): return False
-        for r in range(len(words)):
-            for c in range(len(words)):
-                if c >= len(words[r]) and r >= len(words[c]): continue
-                if c >= len(words[r]) or r >= len(words[c]) or words[r][c] != words[c][r]: return False
+class Solution(object):
+    def validWordSquare(self, words):
+        for i in range(len(words)):
+            for j in range(len(words[i])):
+                if j >= len(words) or i >= len(words[j]) or words[i][j] != words[j][i]:
+                    return False
         return True
