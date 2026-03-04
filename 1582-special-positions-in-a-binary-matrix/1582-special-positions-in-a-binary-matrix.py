63c270c06d48379218ec8a98398655be7b3cff2d
Trung Nguyen <32110646+trungnguyencs@users.noreply.github.com>
2026-03-04 09:19:39 +0700

    Time: 15 ms (11.97%), Space: 19.8 MB (85.11%) - LeetHub

--- 1582-special-positions-in-a-binary-matrix/1582-special-positions-in-a-binary-matrix.py
+++ 1582-special-positions-in-a-binary-matrix/1582-special-positions-in-a-binary-matrix.py
@@ -1,16 +1,14 @@
 class Solution:
     def numSpecial(self, mat: List[List[int]]) -> int:
+        cols, rows = Counter(), Counter()
         ans = 0
-        R, C = len(mat), len(mat[0])
-        rows = [0]*R
-        cols = [0]*C
-        for r in range(R):
-            for c in range(C):
+        for r in range(len(mat)):
+            for c in range(len(mat[0])):
                 if mat[r][c] == 1:
                     rows[r] += 1
                     cols[c] += 1
-        for r in range(R):
-            for c in range(C):
+        for r in range(len(mat)):
+            for c in range(len(mat[0])):
                 if mat[r][c] == rows[r] == cols[c] == 1:
                     ans += 1
         return ans
