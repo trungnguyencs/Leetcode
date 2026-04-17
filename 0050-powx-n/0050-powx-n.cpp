cfcb80ffaf4e064e282a5c994be7e0264ef57482
Trung Nguyen <32110646+trungnguyencs@users.noreply.github.com>
2026-04-17 01:09:52 -0700

    Time: 0 ms (100%), Space: 8.6 MB (43.75%) - LeetHub

--- 0050-powx-n/0050-powx-n.cpp
+++ 0050-powx-n/0050-powx-n.cpp
@@ -1,19 +1,11 @@
 class Solution {
 public:
     double myPow(double x, int n) {
-        if (n == 0) {
-            return 1;
-        };
-        if (n == INT_MIN) {
-            return 1.0/(x * myPow(x, -(n + 1))); //-INT_MIN would cause an overflow error
-        }
-        if (n < 0) {
-            return 1/myPow(x, -n);
-        };
+        if (n == 0) return 1;
+        if (n == INT_MIN) return 1.0/(x * myPow(x, -(n + 1))); //-INT_MIN would cause an overflow error
+        if (n < 0) return 1/myPow(x, -n);
         double tmp = myPow(x, n/2);
-        if (n % 2 == 0) {
-            return tmp * tmp;
-        }
+        if (n % 2 == 0) return tmp * tmp;
         return tmp * tmp * x;
     }
 };
