194af7090a3482103900f46b208dceeb007d5ccc
Trung Nguyen <32110646+trungnguyencs@users.noreply.github.com>
2026-04-16 21:17:16 -0700

    Time: 0 ms (100%), Space: 8.4 MB (85.64%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.cpp
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -1,14 +1,14 @@
 class Solution {
 public:
     int mySqrt(int x) {
-        int l = 0;
-        int r = x/2;
+        int l = 0, r = x;
         while (l <= r) {
-            int m = l + (r - l)/2;
-            if (m * m <= x && (m + 1) * (m + 1) > x) return m;
-            if (m * m < x) l = m + 1;
+            int m = l + (r - l) / 2;
+            long long sq = 1LL * m * m;
+            if (sq == x) return m;
+            if (sq < x) l = m + 1;
             else r = m - 1;
         }
-        return 0;
+        return r;
     }
 };
