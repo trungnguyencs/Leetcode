0cd15de66e7d01cf2038edac58dd8ed7ad268bc1
Trung Nguyen <32110646+trungnguyencs@users.noreply.github.com>
2026-04-22 12:07:29 -0700

    Time: 50 ms (87.13%), Space: 19.7 MB (17.35%) - LeetHub

--- 0281-zigzag-iterator/0281-zigzag-iterator.py
+++ 0281-zigzag-iterator/0281-zigzag-iterator.py
@@ -1,28 +1,22 @@
 class ZigzagIterator:
     def __init__(self, v1: List[int], v2: List[int]):
-        self.i1 = 0
-        self.i2 = 0
-        self.v1 = v1
-        self.v2 = v2
-        self.v1Turn = True
+        self.vectors = [v1, v2]
+        self.pointers = deque() #k vectors: use a queue of pointers
+        if v1:
+            self.pointers.append((0, 0))
+        if v2:
+            self.pointers.append((1, 0))
 
     def next(self) -> int:
-        if self.i2 == len(self.v2):
-            self.v1Turn = True
-        elif self.i1 == len(self.v1):
-            self.v1Turn = False
-        if self.v1Turn:
-            ret = self.v1[self.i1]
-            self.i1 += 1
-        else:
-            ret = self.v2[self.i2]
-            self.i2 += 1
-        self.v1Turn = not self.v1Turn
+        v, i = self.pointers.popleft()
+        ret = self.vectors[v][i]
+        i += 1
+        if i < len(self.vectors[v]):
+            self.pointers.append((v, i))
         return ret
 
     def hasNext(self) -> bool:
-        return self.i1 < len(self.v1) or self.i2 < len(self.v2)
-
+        return len(self.pointers) > 0
 
 # Your ZigzagIterator object will be instantiated and called as such:
 # i, v = ZigzagIterator(v1, v2), []
