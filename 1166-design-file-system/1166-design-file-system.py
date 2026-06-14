cdaa60e6f3079c8a071200ae2ae17475e85a5d41
Trung Nguyen <32110646+trungnguyencs@users.noreply.github.com>
2026-06-13 20:51:49 -0700

    Time: 59 ms (31.55%), Space: 26.8 MB (66.61%) - LeetHub

--- 1166-design-file-system/1166-design-file-system.py
+++ 1166-design-file-system/1166-design-file-system.py
@@ -1,44 +1,41 @@
 class FileSystem:
 
     def __init__(self):
-        self.trie = Trie()
+        self.root = Node(0)
 
-    def createPath(self, path: str, val: int) -> bool:
-        return self.trie.add(self.processPath(path), val)
+    def createPath(self, path: str, value: int) -> bool:
+        folders = self.splitPath(path)
+        cur = self.root
+        for i, folder in enumerate(folders):
+            if i == len(folders) - 1:
+                if folder in cur.children:
+                    return False
+                cur.children[folder] = Node(value)
+            else:
+                if folder not in cur.children:
+                    return False
+                cur = cur.children[folder]
+        return True
 
     def get(self, path: str) -> int:
-        return self.trie.search(self.processPath(path))
-        
-    def processPath(self, path):
-        return path.strip('/').split('/')
-    
-class Node:
-    
-    def __init__(self, val=None):
-        self.children = {}
-        self.val = val
-        
-class Trie:
-    
-    def __init__(self):
-        self.root = Node()
-    
-    def add(self, dirs, val):
+        folders = self.splitPath(path)
         cur = self.root
-        for d in dirs[:-1]:
-            if d not in cur.children: return False
-            cur = cur.children[d]
-        if dirs[-1] in cur.children: return False
-        cur.children[dirs[-1]] = Node(val)
-        return True
-    
-    def search(self, dirs):
-        cur = self.root
-        for d in dirs:
-            if d not in cur.children: return -1
-            cur = cur.children[d]
+        for folder in folders:
+            if folder not in cur.children:
+                return -1
+            cur = cur.children[folder]
         return cur.val
-        
+
+    def splitPath(self, path):
+        return path.split('/')[1:]
+
+class Node:
+
+    def __init__(self, val):
+        self.val = val
+        self.children = {}
+
+
 # Your FileSystem object will be instantiated and called as such:
 # obj = FileSystem()
 # param_1 = obj.createPath(path,value)
