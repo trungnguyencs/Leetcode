13647568f04dd346670cd9ad85918f86cf50ef67
Trung Nguyen <32110646+trungnguyencs@users.noreply.github.com>
2026-06-14 17:24:11 -0700

    Time: 34 ms (76.06%), Space: 21.9 MB (95.54%) - LeetHub

--- 0826-most-profit-assigning-work/0826-most-profit-assigning-work.py
+++ 0826-most-profit-assigning-work/0826-most-profit-assigning-work.py
@@ -1,25 +1,15 @@
 class Solution:
     def maxProfitAssignment(self, difficulty: List[int], profit: List[int], worker: List[int]) -> int:
-        jobs = sorted(zip(difficulty, profit))
-        J, W = len(jobs), len(worker)
+        arr = [(d, p) for d, p in zip(difficulty, profit)]
+        arr.sort() #sort based on job difficulty
+        difficulty = [d for d, _ in arr]
+        profit = [p for _, p in arr]
+        for i in range(1, len(difficulty)):
+            #now profit i in the maxProfit we can get from any job that has difficulty <= difficulty[i]
+            profit[i] = max(profit[i-1], profit[i])
         ans = 0
-        
-        # #approach 1: O(JlogJ + WlogW), no need to update jobs
-        # i = best = 0
-        # for ability in worker:
-        #     while i < J and ability >= jobs[i][0]:
-        #         best = max(best, jobs[i][1])
-        #         i += 1
-        #     ans += best
-        # return ans
-    
-        #approach 2: binary search O(jlogj + wlogj)
-        #have to update jobs (in case of higher effort but lower profit)
-        difficulty, profit = zip(*jobs)
-        profit = list(profit)
-        for i in range(1, J):
-            profit[i] = max(profit[i], profit[i-1])
-        for ability in worker:
-            i = bisect_right(difficulty, ability)
-            if i != 0: ans += profit[i-1]
+        for w in worker:
+            i = bisect_right(difficulty, w)
+            if i == 0: continue #this worker can't do any job
+            ans += profit[i-1]
         return ans
